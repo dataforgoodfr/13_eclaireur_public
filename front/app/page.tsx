c4ead7bfe298e1b6273bf7fac6bd5ed11b9a95bc
theocampbell <57679708+theocampbell@users.noreply.github.com>
2025-02-07 15:15:14 +0100

    Update page.tsx

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className='global-margin flex h-screen flex-col items-center justify-center'>
       <h1>
-        testing with orga changed
+        testing with orga changing
       </h1>
     </div>
   );
