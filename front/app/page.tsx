4bfc900d6d9ee323dae5dbcf21072fb372d4bb09
theocampbell <57679708+theocampbell@users.noreply.github.com>
2025-02-07 11:45:31 +0100

    make change

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,7 +1,12 @@
 export default function Home() {
   return (
     <div className='global-margin flex h-screen flex-col items-center justify-center'>
-      <h1>Welcome to Eclaireur Public</h1>
+      <h1>
+        Ce projet vise à analyser la transparence des collectivités locales concernées par la loi
+        &quot;République Numérique&quot; en cartographiant de manière ponctuelle la publication de certaines
+        données jugées critiques en matière d&apos;intérêt économique ou de probité politique
+        (publication ou non, qualité des publications).
+      </h1>
     </div>
   );
 }
