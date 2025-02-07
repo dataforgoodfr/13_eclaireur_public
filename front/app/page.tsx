90c86dea406e2bcebcf099c13a4153e28d104da5
theocampbell <57679708+theocampbell@users.noreply.github.com>
2025-02-07 15:22:13 +0100

    Update page.tsx

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className='global-margin flex h-screen flex-col items-center justify-center'>
       <h1>
-        stilll testing
+        stilll test
       </h1>
     </div>
   );
