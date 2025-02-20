acf9882ed50f642d2d5dba80371296a8ac2a8518
Riws <thibault.debruyne@hotmail.fr>
2025-02-20 23:23:31 +0100

    feat: chemin hardcodés sirene

--- back/scripts/communities/loaders/sirene.py
+++ back/scripts/communities/loaders/sirene.py
@@ -1,6 +1,4 @@
-from pathlib import Path
 import pandas as pd
-
 from scripts.utils.config import get_project_base_path
 
 
@@ -15,8 +13,8 @@ class SireneLoader:
 
     def __init__(self, config):
         base_path = get_project_base_path()
-        data_folder = Path(base_path / config["path"])
-        self.data = pd.read_csv(data_folder / config["filename"], usecols=config["columns"])
+        payload_path = base_path / config["scrapped_data_file"]
+        self.data = pd.read_csv(payload_path, usecols=config["columns"])
 
     def get(self):
         return self.data
