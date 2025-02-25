4e44fe2078e9a4da35046f3d23f5a18090c65144
theocampbell <57679708+theocampbell@users.noreply.github.com>
2025-02-25 10:05:10 +0100

    update utils location

--- front/components/ui/command.tsx
+++ front/components/ui/command.tsx
@@ -3,7 +3,7 @@
 import * as React from 'react';
 
 import { Dialog, DialogContent } from '@/components/ui/dialog';
-import { cn } from '@/utils';
+import { cn } from '@/utils/utils';
 import { type DialogProps } from '@radix-ui/react-dialog';
 import { Command as CommandPrimitive } from 'cmdk';
 import { Search } from 'lucide-react';
