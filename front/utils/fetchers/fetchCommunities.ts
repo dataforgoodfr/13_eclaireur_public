52e483f7c4664ba6565e97745ca19fc71ef49340
theocampbell <57679708+theocampbell@users.noreply.github.com>
2025-02-25 14:24:00 +0100

    add base url

--- front/utils/fetchers/fetchCommunities.ts
+++ front/utils/fetchers/fetchCommunities.ts
@@ -8,11 +8,16 @@ const DEFAULT_OPTIONS = {
   limit: 5000,
 };
 
+
+
+
 export async function fetchCommunities(options?: Options) {
+  
+  const baseURL =  process.env.BASE_URL
   const limit = options?.limit ?? DEFAULT_OPTIONS.limit;
   const type = options?.type;
 
-  const url = new URL('/api/selected_communities', window.location.host);
+  const url = new URL('/api/selected_communities', baseURL);
 
   if (type) url.searchParams.append('type', type);
   url.searchParams.append('limit', limit.toString());
