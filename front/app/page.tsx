0318c137d5a8c1ed48433fd1e0ee3684289a1946
Benjamin AUBRON <benjamin.aubron@outlook.com>
2025-02-26 17:15:38 +0100

    test map - deleted

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,10 +1,8 @@
-import Map from "@/components/Map"
 
 export default function Home() {
   return (
     <div className='flex h-screen flex-col items-center justify-center'>
       <h1>Welcome to Eclaireur Public</h1>
-      <Map/>
     </div>
   );
 }
