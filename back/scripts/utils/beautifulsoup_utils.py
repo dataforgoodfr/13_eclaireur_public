3cf29812579a43216586d552653f263306507a02
Riws <thibault.debruyne@hotmail.fr>
2025-03-03 20:16:28 +0100

    chore: beautifulsoup_utils typing

--- back/scripts/utils/beautifulsoup_utils.py
+++ back/scripts/utils/beautifulsoup_utils.py
@@ -1,9 +1,9 @@
 from datetime import datetime
 
-from bs4 import BeautifulSoup
+from bs4.element import Tag
 
 
-def get_tag_datetime(tag: BeautifulSoup) -> datetime:
+def get_tag_datetime(tag: Tag | None) -> datetime | None:
     if tag and tag.text:
         fmt = "%d/%m/%Y"
         if " " in tag.text:
@@ -12,18 +12,19 @@ def get_tag_datetime(tag: BeautifulSoup) -> datetime:
     return None
 
 
-def get_tag_text(tag: BeautifulSoup) -> str | None:
+def get_tag_text(tag: Tag | None) -> str | None:
+    # TODO: [Données non publiées], trop spécifique pour être dans utils
     if tag and tag.text and (tag.text != "[Données non publiées]"):
         return tag.text
     return None
 
 
-def get_tag_bool(tag: BeautifulSoup) -> str | None:
+def get_tag_bool(tag: Tag | None) -> bool | None:
     txt = get_tag_text(tag)
-    return txt and (txt == "true")
+    return txt == "true"
 
 
-def get_tag_float(tag: BeautifulSoup) -> float | None:
+def get_tag_float(tag: Tag | None) -> float | None:
     if tag and tag.text:
         return float(
             tag.text.replace(" ", "")
@@ -34,7 +35,7 @@ def get_tag_float(tag: BeautifulSoup) -> float | None:
     return None
 
 
-def get_tag_int(tag: BeautifulSoup) -> int | None:
+def get_tag_int(tag: Tag | None) -> int | None:
     f = get_tag_float(tag)
     if f is not None:
         return int(f)
