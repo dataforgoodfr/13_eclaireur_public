173b0e009dc6d78ec52a8421c8fb7aba14a1a1d9
OctaveLT <ole_tullier@orange.fr>
2025-03-10 13:00:02 +0100

    feat: add models for community, marchePublic and subvention

---
+++ front/app/models/community.ts
@@ -0,0 +1,16 @@
+export type Community = {
+  /** Primary key [char9] */
+  siren: string;
+  /** Primary key */
+  type: string;
+  cog: string;
+  code_departement: string;
+  code_region: string;
+  epci: string;
+  latitude: number;
+  longitude: number;
+  population: number;
+  superficie: number;
+  obligation_publication: boolean;
+  nom_elu: string;
+};
