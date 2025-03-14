92fef376cb0a5bc4f94af67e8195b8227bc68886
Antoine Quesnel (perso) <antoine.quesnel@hotmail.fr>
2025-03-14 15:50:05 +0100

    [FRONT] feature: Maj Navbar

--- front/components/navbar.tsx
+++ front/components/navbar.tsx
@@ -1,13 +1,19 @@
+import { Share2 } from 'lucide-react';
 import Link from 'next/link';
 
 export default function Navbar() {
   return (
-    <div className='sticky top-0 z-50 w-full bg-blue-500'>
-      <Link href='/'>
-        <div className='global-margin h-full px-4 py-10 text-2xl font-semibold'>
-          Eclaireur Public
+    <div className='container mx-auto flex items-center px-6 py-2 h-16'>
+      <h1 className='text-lg font-bold'>Eclaireur Public</h1>
+      <div className='grow'>
+        <div className='flex items-center justify-end gap-2 md:gap-8 uppercase'>
+          <Link href='/'>Visualiser</Link>
+          <Link href='/'>Comprendre</Link>
+          <Link href='/'>Télécharger</Link>
+          <Link className='border-l pl-2 md:pl-8 border-gray-500' href='/'>À propos</Link>
+          <Share2 />
         </div>
-      </Link>
+      </div>
     </div>
   );
 }
