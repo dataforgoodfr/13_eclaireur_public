b6e960bfddfffdc58ecf4dee08ecdb3152436a2b
Christophe Goudet <goudetdata@gmail.com>
2025-03-15 11:59:42 +0100

    Adapt for CI

--- back/scripts/datasets/datagouv_catalog.py
+++ back/scripts/datasets/datagouv_catalog.py
@@ -25,14 +25,27 @@ class DataGouvCatalog:
         if self.filename.exists():
             return
 
+        url = self.config.get("catalog_url", self._catalog_url())
+
+        df = CSVLoader(url).load()
+        if not isinstance(df, pd.DataFrame):
+            raise RuntimeError("Failed to load dataset")
+        df.to_parquet(self.filename)
+
+    def _catalog_url(self):
+        """
+        Fetch the url of the resource catalog from the page of the dataset.
+        The catalog is updated daily, with a title change so there is high chance
+        that the resource_id we are interested changes daily too.
+
+        The page itself allows to download a parquet which is dynamically created.
+        It weights 7x less but does not appear on the catalog and seems to have a different url daily.
+        See the page for investigation : https://www.data.gouv.fr/fr/datasets/catalogue-des-donnees-de-data-gouv-fr/#
+        """
         catalog_dataset = DataGouvAPI.dataset_resources(
             self.DATASET_ID, savedir=self.data_folder
         ).pipe(lambda df: df[df["resource_url"].str.contains("export-resource")])
         if catalog_dataset.empty:
             raise Exception("No catalog dataset found.")
 
-        url = catalog_dataset["resource_url"].iloc[0]
-        df = CSVLoader(url).load()
-        if not isinstance(df, pd.DataFrame):
-            raise RuntimeError("Failed to load dataset")
-        df.to_parquet(self.filename)
+        return catalog_dataset["resource_url"].iloc[0]
