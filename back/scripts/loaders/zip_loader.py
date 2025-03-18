680492d422822270872a528eb66b41acdbdf6ee7
Riws <thibault.debruyne@hotmail.fr>
2025-03-18 23:16:26 +0100

    feat: complete ZipLoader._loader_class_resolver_from_url

--- back/scripts/loaders/zip_loader.py
+++ back/scripts/loaders/zip_loader.py
@@ -27,7 +27,7 @@ class ZipLoader(BaseLoader):
             loader_class = BaseLoader.loader_factory(self.output_file_path, **self.kwargs)
         else:
             loader_class = self.archived_file_loader_class(self.output_file_path, **self.kwargs)
-        return loader_class.load(force=False)
+        return loader_class.load()
 
     def _load_from_url(self):
         with tempfile.TemporaryDirectory() as tempdir:
@@ -39,19 +39,6 @@ class ZipLoader(BaseLoader):
             return self._load_from_file()
 
     def _load_from_file(self):
-        def get_archive_prefix() -> str:
-            """
-            Get the archive prefix from the file_url, e.g. "path/file.zip" -> "file"
-            """
-            file_url = self.file_url
-            if not self.get_file_is_url(file_url):
-                file_url = file_url.rsplit("/", 1)[-1]
-            for extension in self.file_extensions or ():
-                if file_url.endswith(f".{extension}"):
-                    file_url = file_url.removesuffix(f".{extension}")
-                    break
-            return file_url
-
         with tempfile.TemporaryDirectory() as tmpdir, zipfile.ZipFile(self.file_url) as zip_ref:
             if self.archived_file_loader_class is None:
                 can_be_loaded_filenames = zip_ref.namelist()
@@ -69,7 +56,7 @@ class ZipLoader(BaseLoader):
                     filename = can_be_loaded_filenames[0]
                 case _:
                     # Too many files but if we're lucky, the file we're looking for has the same prefix as its archive
-                    archive_filename = get_archive_prefix()
+                    archive_filename = self.get_archive_prefix()
                     can_be_loaded_filenames = [
                         filename
                         for filename in can_be_loaded_filenames
@@ -85,6 +72,20 @@ class ZipLoader(BaseLoader):
             self.output_file_path = Path(tmpdir) / filename
             return self.process_data()
 
+    def get_archive_prefix(self) -> str:
+        """
+        Get the archive prefix from the file_url, e.g. "path/file.zip" -> "file"
+        """
+        file_url = self.file_url
+        if self.get_file_is_url(file_url):
+            file_url = urlparse(file_url).path
+        file_url = file_url.rsplit("/", 1)[-1]
+        for extension in self.file_extensions or ():
+            if file_url.endswith(f".{extension}"):
+                file_url = file_url.removesuffix(f".{extension}")
+                break
+        return file_url
+
     def _loader_class_resolver_from_url(self) -> Type | None:
         """
         Resolve the archive file loader class based on the URL path.
@@ -92,11 +93,17 @@ class ZipLoader(BaseLoader):
         # TODO: LOADER_CLASSES may be more effective as BaseLoader staticmethod
         from back.scripts.loaders import LOADER_CLASSES
 
+        # First, try to find the archive file loader class based on the filename
+        filename = self.get_archive_prefix()
+        filename_extension = filename.rsplit(".", 1)[-1]
+        if filename_extension in LOADER_CLASSES:
+            return LOADER_CLASSES[filename_extension]
+
+        # Second, try to find the archive file loader class based on the url path
         url_path = urlparse(self.file_url).path
         # TODO: more robust method?
-        url_path_parts = set(url_path.split("/"))
-
-        potential_extentions = url_path_parts & LOADER_CLASSES.keys()
+        url_path_parts = url_path.split("/")
+        potential_extentions = set(url_path_parts) & LOADER_CLASSES.keys()
         match len(potential_extentions):
             case 0:
                 LOGGER.warning(
