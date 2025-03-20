0340da4ad67d827e902d883e28f0c1ce0deae4e6
OctaveLT <ole_tullier@orange.fr>
2025-03-20 18:42:52 +0100

    fix: forgotten import

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,4 +1,4 @@
-import HomePageHeader from '@/components/HomepageHeader';
+import HomePageHeader from '@/components/HomePageHeader';
 import CtaGroup from '@/components/cta/CtaGroup';
 
 export default async function Home() {
