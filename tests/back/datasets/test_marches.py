d6a79a2a169af33acbed42b010c47b3b7c3ac798
Arnaud Odet <arnaud.odet.pro@gmail.com>
2025-03-24 14:50:23 +0100

    [BACK] Marches - Adding method to handle different .json structure and corresponding tests

--- tests/back/datasets/test_marches.py
+++ tests/back/datasets/test_marches.py
@@ -1,54 +1,24 @@
 from back.scripts.datasets.marches import MarchesPublicsWorkflow
-import json
 import os
+from pathlib import Path
 
 
-nested_json = {
-    "marches": {
-        "marche": [
-            {
-                "id": "01",
-                "acheteur": {"id": "a_01"},
-                "montant": 500.0,
-                "titulaires": [{"titulaire": {"typeIdentifiant": "SIRET", "id": "id_1"}}],
-            },
-            {
-                "id": "02",
-                "acheteur": {"id": "a_02"},
-                "montant": 12.78,
-                "titulaires": [{"titulaire": {"typeIdentifiant": "SIRET", "id": "id_2"}}],
-            },
-        ]
-    }
-}
-
-direct_json = {
-    "marches": [
-        {
-            "id": "01",
-            "acheteur": {"id": "a_01"},
-            "montant": 500.0,
-            "titulaires": [{"titulaire": {"typeIdentifiant": "SIRET", "id": "id_1"}}],
-        },
-        {
-            "id": "02",
-            "acheteur": {"id": "a_02"},
-            "montant": 12.78,
-            "titulaires": [{"titulaire": {"typeIdentifiant": "SIRET", "id": "id_2"}}],
-        },
-    ]
-}
+FIXTURES_DIRECTORY = Path(__file__).parent / "fixtures"
 
 
 def test_direct_json_structure():
-    with open("test.json", "w") as f:
-        json.dump(direct_json, f)
-    assert MarchesPublicsWorkflow.check_json_structure("test.json") == "direct"
-    os.remove("test.json")
+    assert (
+        MarchesPublicsWorkflow.check_json_structure(
+            os.path.join(FIXTURES_DIRECTORY, "marche_direct.json")
+        )
+        == "direct"
+    )
 
 
 def test_nested_json_structure():
-    with open("test.json", "w") as f:
-        json.dump(nested_json, f)
-    assert MarchesPublicsWorkflow.check_json_structure("test.json") == "nested"
-    os.remove("test.json")
+    assert (
+        MarchesPublicsWorkflow.check_json_structure(
+            os.path.join(FIXTURES_DIRECTORY, "marche_nested.json")
+        )
+        == "nested"
+    )
