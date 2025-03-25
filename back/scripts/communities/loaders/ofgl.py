1d5fdae4837c7b28eff3de39feebdcd18070d19e
Christophe Goudet <goudetdata@gmail.com>
2025-03-25 22:44:08 +0100

    vlaid ofgl

--- back/scripts/communities/loaders/ofgl.py
+++ back/scripts/communities/loaders/ofgl.py
@@ -12,7 +12,18 @@ from back.scripts.utils.decorators import tracker
 
 LOGGER = logging.getLogger(__name__)
 
-COM_CSV_DTYPES = {"Code Siren 2023 EPCI": str, "Code Siren Collectivité": str}
+COM_CSV_DTYPES = {
+    "Code Siren Collectivité": str,
+    "Code Insee Collectivité": str,
+}
+READ_COLUMNS = {
+    "Exercice": None,
+    "Outre-mer": None,
+    "Catégorie": "categorie",
+    "Population totale": "population",
+    "Code Insee Collectivité": "code_insee",
+    "Code Siren Collectivité": "siren",
+}
 
 
 class OfglLoader(DatasetAggregator):
@@ -21,23 +32,33 @@ class OfglLoader(DatasetAggregator):
         files = pd.read_csv(config["urls_csv"], sep=";")
         return cls(files, config)
 
+    def __init__(self, files: pd.DataFrame, config: dict):
+        super().__init__(files, config)
+        self.columns = pd.DataFrame()
+
     @tracker(ulogger=LOGGER, inputs=True)
     def _read_parse_file(self, file_metadata: tuple, raw_filename: Path) -> pd.DataFrame | None:
         # Because parquet format seems buggy on the platform for commune;
         # We are forced to use csv which need some typing help
-        opts = {"dtype": COM_CSV_DTYPES} if file_metadata.format == "csv" else {}
+        opts = {"columns": READ_COLUMNS.keys(), "dtype": COM_CSV_DTYPES}
+
         loader = LOADER_CLASSES[file_metadata.format](raw_filename, **opts)
         df = (
             loader.load()
+            .rename(columns={k: v for k, v in READ_COLUMNS.items() if v})
             .pipe(normalize_column_names)
             .assign(
                 type=file_metadata.code,
                 outre_mer=lambda df: (df["outre_mer"] == "Oui").fillna(False),
             )
+            .sort_values("exercice", ascending=False)
+            .drop_duplicates(subset=["siren"], keep="first")
         )
-        if "reg_is_ctu" in df.columns:
-            df = df.assign(reg_is_ctu=(df["reg_is_ctu"] == "Oui").fillna(False))
 
+        self.columns = pd.concat(
+            [self.columns, pd.DataFrame({file_metadata.code: 1}, index=df.columns)], axis=1
+        )
+        self.columns.to_csv(self.data_folder / "columns.csv")
         return df
 
     def get(self):
