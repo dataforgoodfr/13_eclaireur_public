6494fbd6cbcd313701072fe3aef0b986525fa7a4
OctaveLT <ole_tullier@orange.fr>
2025-03-27 12:19:28 +0100

    fix: add CommunitiesTest table

--- front/utils/fetchers/constants.ts
+++ front/utils/fetchers/constants.ts
@@ -2,4 +2,5 @@ export enum DataTable {
   Communities = 'staging_communities',
   Subventions = 'subventions_normalized_data',
   MarchesPublics = 'staging_marches_public',
+  CommunitiesTest = 'selected_communitiestest_indices',
 }
