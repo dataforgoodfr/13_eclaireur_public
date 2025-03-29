4c6877465c3b954f05ac6dc5c3344ec083f9c9a9
Christophe Goudet <goudetdata@gmail.com>
2025-03-29 12:07:39 +0100

    working test

---
+++ tests/back/datasets/test_topic_aggregator.py
@@ -0,0 +1,69 @@
+import os
+import tempfile
+from pathlib import Path
+
+import pandas as pd
+
+from back.scripts.datasets.topic_aggregator import TopicAggregator
+
+FIXTURES_PATH = Path(__file__).parent / "fixtures"
+
+
+class TestTopicAggregator:
+    def setup_method(self):
+        self.path = tempfile.TemporaryDirectory()
+        self.config = {
+            "data_folder": self.path.name,
+            "combined_filename": os.path.join(self.path.name, "final.parquet"),
+        }
+        self.topic_config = {
+            "schema": {"url": "file:" + str(FIXTURES_PATH / "subvention_schema.json")},
+            "schema_dict_file": "back/data/datasets/subventions/inputs/dataset_dict.csv",
+        }
+        self.raw_filename = Path(self.path.name) / "raw.csv"
+        self.files_in_scope = pd.DataFrame(
+            {
+                "url": ["file:" + str(self.raw_filename)],
+                "format": "csv",
+                "siren": "123456789",
+                "type": "COM",
+            }
+        )
+
+    def teardown_method(self):
+        self.path.cleanup()
+
+    def test_minimal_benef_only(
+        self,
+    ):
+        inp = pd.DataFrame(
+            {
+                "raison_sociale": ["A", "B"],
+                "siret": ["12345678901234", "12345678987094"],
+                "total_montant_en_euros": [5, 100],
+            }
+        )
+        inp.to_csv(self.raw_filename, index=False)
+        aggregator = TopicAggregator(
+            files_in_scope=self.files_in_scope,
+            topic="test_topic",
+            topic_config=self.topic_config,
+            datafile_loader_config=self.config,
+        )
+
+        aggregator.run()
+
+        out = pd.read_parquet(self.config["combined_filename"])
+        print(out)
+        expected = pd.DataFrame(
+            {
+                "nomBeneficiaire": ["A", "B"],
+                "idBeneficiaire": ["12345678901234", "12345678987094"],
+                "montant": [5.0, 100],
+                "topic": "test_topic",
+                "url": "file:" + str(self.raw_filename),
+                "coll_type": "COM",
+                "idAttribuant": "12345678900000",
+            }
+        )
+        pd.testing.assert_frame_equal(out, expected)
