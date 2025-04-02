b623bc65f56d2ebd66245250b3fc0e8646dbbd52
Benjamin AUBRON <benjamin.aubron@outlook.com>
2025-04-02 22:54:32 +0200

    top10 done

--- front/app/community/[siren]/components/FicheMarchesPublics/FicheMarchesPublics.tsx
+++ front/app/community/[siren]/components/FicheMarchesPublics/FicheMarchesPublics.tsx
@@ -2,34 +2,13 @@ import { MarchePublic } from '@/app/models/marche_public';
 
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import Trends from './Trends';
+import Top10 from './Top10';
 
 type FicheMarchesPublics = {
   marchesPublics: MarchePublic[];
 };
 
 export function FicheMarchesPublics({ marchesPublics }: FicheMarchesPublics) {
-
-  // // MarketType data
-  // const marketType = Object.values(
-  //   marchesPublics.reduce((acc, item) => {
-  //     if (!acc[item.nature]) {
-  //       acc[item.nature] = { name: item.nature, value: 0 };
-  //     }
-  //     acc[item.nature].value += 1;
-  //     return acc;
-  //   }, {}),
-  // );
-
-  // // MarketProcess data
-  // const marketProcess = Object.values(
-  //   marchesPublics.reduce((acc, item) => {
-  //     if (!acc[item.procedure]) {
-  //       acc[item.procedure] = { name: item.procedure, value: 0 };
-  //     }
-  //     acc[item.procedure].value += parseFloat(item.montant);
-  //     return acc;
-  //   }, {}),
-  // );
 
   return (
     <div className='mx-auto my-6 max-w-screen-2xl rounded-xl border p-6 shadow'>
@@ -41,13 +20,13 @@ export function FicheMarchesPublics({ marchesPublics }: FicheMarchesPublics) {
           <TabsTrigger value="details">Top 10</TabsTrigger>
         </TabsList>
         <TabsContent value="trends">
-          <Trends  data={marchesPublics} />
+          <Trends data={marchesPublics} />
         </TabsContent>
         <TabsContent value="distribution">
           <div className="bg-neutral-200 w-full h-[700px] flex items-center justify-center">A construire</div>
         </TabsContent>
         <TabsContent value="details">
-          <div className="bg-neutral-200 w-full h-[700px]"></div>
+          <Top10 rawData={marchesPublics} />
         </TabsContent>
       </Tabs>
     </div>
---
+++ front/app/community/[siren]/components/FicheMarchesPublics/Top10.tsx
@@ -0,0 +1,231 @@
+'use client'
+
+import React, { use } from 'react'
+import { useEffect, useState } from 'react'
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
+import { Switch } from '@/components/ui/switch'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+import { ArrowDownToLine } from 'lucide-react'
+import { MarchePublic } from '@/app/models/marche_public'
+
+
+export default function Top10({ rawData }: { rawData: any[] }) {
+
+  const [categoriesDisplayed, setCategoriesDisplayed] = useState(false)
+  const [selectedYear, setSelectedYear] = useState('All')
+
+  // Selecter
+  const availableYears = [...new Set(rawData.map((item) => item.datenotification_annee))].sort(
+    (a, b) => Number(a) - Number(b),
+  )
+
+  let filteredData : MarchePublic[]
+  if (selectedYear === 'All') {
+    filteredData = rawData
+  } else {
+    filteredData = rawData.filter((item) => item.datenotification_annee === selectedYear)
+  }
+
+  function formatNumberWithSpaces(number: number): string {
+    return number.toLocaleString('fr-FR')
+  }
+
+  function formatCompanies(input: string): string[] {
+    return input
+      .replace(/[\[\]]/g, '')
+      .split(/',\s*'|",\s*"/) 
+      .map(item => item.replace(/^'|-'?$/g, '').trim())
+}
+  
+
+  function getTop10Contract(data: any[]) {
+
+    const sortedContracts = data
+    .sort((a, b) => Number(b.montant) - Number(a.montant))
+    let Top10Contract = []
+    if (sortedContracts.length > 10) {
+      Top10Contract = sortedContracts.slice(0, 10)
+    }
+    else {
+      Top10Contract = sortedContracts
+    }
+
+    return Top10Contract
+  }
+
+  function getTop10Sector(data: any[]) {
+
+    const groupedData = data.reduce(
+      (acc, { cpv_2_label, montant }) => {
+        if (!acc[cpv_2_label]) {
+          acc[cpv_2_label] = 0
+        }
+        acc[cpv_2_label] += parseFloat(montant)
+        return acc
+      },
+      {} as Record<string, number>,
+    )
+
+    const total = filteredData.reduce((acc, item) => acc + parseFloat(item.montant), 0)
+    const top1 = Math.max(...Object.values(groupedData).map(Number))
+
+    const formattedData = Object.entries(groupedData)
+      .map(([name, size]) => ({ name, size }))
+      .sort((a, b) => Number(b.size) - Number(a.size));
+
+    const formattedPlusTotal = formattedData.map((item) => ({
+      ...item,
+      part: Math.round((Number(item.size) / total) * 100 * 10) / 10,
+      pourcentageCategoryTop1: Math.round((Number(item.size) / top1) * 100 * 10) / 10,
+    }));
+
+    let sortedData;
+    if (formattedPlusTotal.length > 10) {
+      sortedData = formattedPlusTotal.slice(0, 10);
+    } else {
+      sortedData = formattedPlusTotal;
+    }
+
+    return sortedData;
+  }
+
+  const getTop10SectorData = getTop10Sector(filteredData);
+  const getTop10ContractData = getTop10Contract(filteredData);
+
+
+  return (
+    <>
+      <div className='flex items-center justify-between'>
+        <div className='flex items-baseline gap-2'>
+          <h3 className='py-2 text-xl'>Top 10 </h3>
+          <div className='flex items-baseline gap-2'>
+            <div
+              onClick={() => setCategoriesDisplayed(false)}
+              className={`cursor-pointer ${!categoriesDisplayed ? 'text-neutral-800' : 'text-neutral-400'}`}
+            >
+              (des contrats
+            </div>
+            <Switch
+              checked={categoriesDisplayed}
+              onCheckedChange={() => setCategoriesDisplayed((prev) => !prev)}
+            />
+            <div
+              onClick={() => setCategoriesDisplayed(true)}
+              className={`cursor-pointer ${categoriesDisplayed ? 'text-neutral-800' : 'text-neutral-400'}`}
+            >
+              par secteurs)
+            </div>
+          </div>
+        </div>
+        <div className='flex items-center gap-2'>
+          <Select onValueChange={(value) => setSelectedYear(value)}>
+            <SelectTrigger className='w-[100px]'>
+              <SelectValue placeholder='Tout voir' />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value='All'>Tout voir</SelectItem>
+              {availableYears.map((year) => (
+                <SelectItem key={year} value={year}>
+                  {year}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <div className='rounded p-1 hover:bg-neutral-100'>
+                <ArrowDownToLine className='text-neutral-500' />
+              </div>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuItem>Télécharger les données</DropdownMenuItem>
+              <DropdownMenuItem>Télécharger le graphique</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      </div>
+      {categoriesDisplayed && (
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className='w-[400px]'>Secteur</TableHead>
+              <TableHead className='w-[700px]'>Montant</TableHead>
+              <TableHead className=''></TableHead>
+              <TableHead className='text-right'>Part</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {getTop10SectorData.map((item, index) => (
+              <TableRow key={index}>
+                <TableCell className='font-medium'>{item.name}</TableCell>
+                <TableCell>
+                  <div className='relative h-2 w-full rounded-md'>
+                    <div
+                      className='h-2 rounded-md bg-blue-500'
+                      style={{ width: `${item.pourcentageCategoryTop1}%` }}
+                    ></div>
+                  </div>
+                </TableCell>
+                <TableCell>{formatNumberWithSpaces(Number(item.size))} €</TableCell>
+                <TableCell className='text-right'>{`${item.part}%`}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      )}
+      {!categoriesDisplayed && (
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className='w-[300px]'>Titulaires</TableHead>
+              <TableHead className=''>Objet</TableHead>
+              <TableHead className='w-[140px] text-right'>Montant</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {getTop10ContractData.map((item, index) => (
+              <TableRow key={index}>
+                <TableCell className='font-medium space-x-1'>
+                  {formatCompanies(item.titulaires_liste_noms).map((company, index) => (
+                    <span 
+                      key={index}
+                      className="bg-neutral-200 px-2 py-.5 rounded-md"
+                    >
+                      {company}
+                    </span>
+                  ))}
+                </TableCell>
+                <TableCell className=''>
+                  {item.objet}
+                </TableCell>
+                <TableCell className='text-right'>
+                  {formatNumberWithSpaces(Number(item.montant))} €
+                </TableCell>
+              </TableRow>)
+            )}
+          </TableBody>
+        </Table>
+      )}
+    </>
+  )
+}
