8aa900afbc391cf08cf6dabe9aafa6c84a4ea181
Oualter <olivier.pretre@gmx.fr>
2025-04-04 17:39:24 +0200

    remove comments

--- front/next.config.ts
+++ front/next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
     remotePatterns: [
       {
@@ -9,7 +8,6 @@ const nextConfig: NextConfig = {
         hostname: 'placehold.co',
       },
     ],
-    // disableStaticImages: true,
   },
 };
 
