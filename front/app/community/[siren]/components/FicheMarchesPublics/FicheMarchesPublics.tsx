acbf0e29724d806704a872db8ed12645e888318b
Benjamin AUBRON <benjamin.aubron@outlook.com>
2025-04-04 22:16:42 +0200

    DRY - noData-MarchesPublics

--- front/app/community/[siren]/components/FicheMarchesPublics/FicheMarchesPublics.tsx
+++ front/app/community/[siren]/components/FicheMarchesPublics/FicheMarchesPublics.tsx
@@ -15,47 +15,48 @@ type FicheMarchesPublics = {
   marchesPublics: MarchePublic[];
 };
 
+function NoData() {
+  return (
+    <div className='flex h-[600px] w-full items-center justify-center bg-neutral-100'>
+      Aucune donnée n'a été publiée
+    </div>
+  );
+}
+
 export async function FicheMarchesPublics({ siren }: { siren: string }) {
   const marchesPublics = await getMarchesPublics(siren);
-
-  if (marchesPublics.length === 0) {
-    return (
-      <div className='mx-auto my-6 max-w-screen-2xl rounded-xl border p-6 shadow'>
-        <h2 className='pb-3 text-center text-2xl'>Marchés Publics</h2>
-        <div className='flex h-[600px] w-full items-center justify-center bg-neutral-100'>
-          Aucune donnée n'a été publiée
-        </div>
-      </div>
-    );
-  }
 
   return (
     <div className='mx-auto my-6 max-w-screen-2xl rounded-xl border p-6 shadow'>
       <h2 className='pb-3 text-center text-2xl'>Marchés Publics</h2>
-      <Tabs defaultValue='trends'>
-        <TabsList>
-          <TabsTrigger value='trends'>Évolution</TabsTrigger>
-          <TabsTrigger value='distribution'>Répartition</TabsTrigger>
-          <TabsTrigger value='compare'>Comparaison</TabsTrigger>
-          <TabsTrigger value='details'>Classement</TabsTrigger>
-        </TabsList>
-        <TabsContent value='trends'>
-          <Trends data={marchesPublics} />
-        </TabsContent>
-        <TabsContent value='distribution'>
-          <div className='flex h-[600px] w-full items-center justify-center bg-neutral-200'>
-            Treemap en construction
-          </div>
-        </TabsContent>
-        <TabsContent value='compare'>
-          <div className='flex h-[600px] w-full items-center justify-center bg-neutral-200'>
-            En construction
-          </div>
-        </TabsContent>
-        <TabsContent value='details'>
-          <Top10 rawData={marchesPublics} />
-        </TabsContent>
-      </Tabs>
+      {marchesPublics.length > 0 ? (
+        <Tabs defaultValue='trends'>
+          <TabsList>
+            <TabsTrigger value='trends'>Évolution</TabsTrigger>
+            <TabsTrigger value='distribution'>Répartition</TabsTrigger>
+            <TabsTrigger value='compare'>Comparaison</TabsTrigger>
+            <TabsTrigger value='details'>Classement</TabsTrigger>
+          </TabsList>
+          <TabsContent value='trends'>
+            <Trends data={marchesPublics} />
+          </TabsContent>
+          <TabsContent value='distribution'>
+            <div className='flex h-[600px] w-full items-center justify-center bg-neutral-200'>
+              Treemap en construction
+            </div>
+          </TabsContent>
+          <TabsContent value='compare'>
+            <div className='flex h-[600px] w-full items-center justify-center bg-neutral-200'>
+              En construction
+            </div>
+          </TabsContent>
+          <TabsContent value='details'>
+            <Top10 rawData={marchesPublics} />
+          </TabsContent>
+        </Tabs>
+      ) : (
+        <NoData />
+      )}
     </div>
   );
 }
