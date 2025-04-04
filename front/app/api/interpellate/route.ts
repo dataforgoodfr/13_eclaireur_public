659767926508e0af0b4a8f60d7daac14b2e9546f
Oualter <olivier.pretre@gmx.fr>
2025-04-04 12:14:01 +0200

    fixes on interpellate route.ts

--- front/app/api/interpellate/route.ts
+++ front/app/api/interpellate/route.ts
@@ -1,73 +1,67 @@
-import { formSchema } from 'utils/types'
-import { NextResponse } from 'next/server'
-import nodemailer from 'nodemailer'
-import Mail from 'nodemailer/lib/mailer'
+import { NextResponse } from 'next/server';
+
+import nodemailer from 'nodemailer';
+import Mail from 'nodemailer/lib/mailer';
+import { formSchema } from 'utils/types';
+
+const HOST_ACCOUNT_ADDRESS = 'mail.gmx.com';
+const HOST_ACCOUNT_EMAIL = process.env.MY_EMAIL;
+const HOST_ACCOUNT_PASSWORD = process.env.MY_PASSWORD;
+const HOST_ACCOUNT_EMAIL_CARBON_COPY = process.env.CC_EMAIL_MESSAGE;
 
 export async function POST(request: Request) {
-  const body: unknown = await request.json()
-
-  const result = formSchema.safeParse(body)
-  type TResultData = {
-    firstname: string
-    lastname: string
-    email: string
-    to: string[]
-    object: string
-    message: string
-  }
-  const { success, data } = result
-  let firstname, lastname, email, to, object, message
+  const body: unknown = await request.json();
+  const result = formSchema.safeParse(body);
+  const { success, data } = result;
+  let firstname, lastname, email, to, object, message;
   if (success && data) {
-    ;({ firstname, lastname, email, to, object, message } = data)
+    ({ firstname, lastname, email, to, object, message } = data);
   }
-  //   const { firstname, lastname, email, to, object, message } = data
-  console.log('firstname => ', firstname)
-
-  // check out Zod's .flatten() method for an easier way to process errors
-  let zodErrors = {}
+  // TODO - check out Zod's .flatten() method for an easier way to process errors
+  let zodErrors = {};
   if (!result.success) {
     result.error.issues.forEach((issue) => {
-      zodErrors = { ...zodErrors, [issue.path[0]]: issue.message }
-    })
+      zodErrors = { ...zodErrors, [issue.path[0]]: issue.message };
+    });
   }
 
   if (Object.keys(zodErrors).length > 0) {
-    return NextResponse.json({ errors: zodErrors })
+    return NextResponse.json({ errors: zodErrors });
   }
 
   const transport = nodemailer.createTransport({
-    host: 'mail.gmx.com',
+    host: HOST_ACCOUNT_ADDRESS,
     port: 465,
     secure: true,
     auth: {
-      user: process.env.MY_EMAIL,
-      pass: process.env.MY_PASSWORD,
+      user: HOST_ACCOUNT_EMAIL,
+      pass: HOST_ACCOUNT_PASSWORD,
     },
-  })
+  });
 
   const mailOptions: Mail.Options = {
-    from: process.env.MY_EMAIL,
-    to: process.env.MY_EMAIL,
-    cc: process.env.CC_EMAIL_MESSAGE,
+    from: HOST_ACCOUNT_EMAIL,
+    to: HOST_ACCOUNT_EMAIL,
+    cc: HOST_ACCOUNT_EMAIL_CARBON_COPY,
     subject: `|| ECLAIREUR PUBLIC || Message de ${firstname} ${lastname} (${email})`,
     html: message,
-  }
+  };
 
   const sendMailPromise = () =>
     new Promise<string>((resolve, reject) => {
       transport.sendMail(mailOptions, function (err) {
         if (!err) {
-          resolve('Email sent')
+          resolve('Email sent');
         } else {
-          reject(err.message)
+          reject(err.message);
         }
-      })
-    })
+      });
+    });
 
   try {
-    await sendMailPromise()
-    return NextResponse.json({ message: 'Email sent' })
+    await sendMailPromise();
+    return NextResponse.json({ message: 'Email sent' });
   } catch (err) {
-    return NextResponse.json({ error: err }, { status: 500 })
+    return NextResponse.json({ error: err }, { status: 500 });
   }
 }
