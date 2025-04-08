fd1920747ab1098a73a5affa9ed4b0acf82ecdd2
Oualter <olivier.pretre@gmx.fr>
2025-04-08 12:22:23 +0200

    rankingCommunity fix

--- front/components/Communities/RankingCommunity.tsx
+++ front/components/Communities/RankingCommunity.tsx
@@ -1,7 +1,7 @@
-type TCommunityName = {
+type RankingCommunityProps = {
   communityName: string;
 };
-export default function RankingCommunity({ communityName }: TCommunityName) {
+export default function RankingCommunity({ communityName }: RankingCommunityProps) {
   return (
     <div className='right max-w-[300] px-4 py-2 font-bold'>
       10e sur 120 villes de plus de 100 000 habitants
