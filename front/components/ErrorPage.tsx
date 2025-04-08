3d4c2455d017bdba895742c573dc00c73a108e90
Antoine Quesnel (perso) <antoine.quesnel@hotmail.fr>
2025-04-08 13:32:05 +0200

    feat: add error pages 404 and 500

---
+++ front/components/ErrorPage.tsx
@@ -0,0 +1,32 @@
+import Link from 'next/link';
+import Image from 'next/image'
+import { Button } from '@/components/ui/button';
+
+type ErrorPageProps = {
+  title: string;
+  description: string;
+  imagePath: string;
+  displayContact: boolean;
+};
+
+
+export default function ErrorPage({ title, description, imagePath, displayContact }: ErrorPageProps) {
+  return (
+    <div className='grid h-[600px] place-content-center justify-items-center bg-white px-4'>
+    <Image
+    src={imagePath}
+    width={400}
+    height={400}
+    alt=""
+    className=''
+  />
+      <h1 className='text-xl text-gray-700'>{title}</h1>
+      <p className='text-gray-800'>
+        {description}
+      </p>
+      <Link href={'/'}>
+        <Button className='mt-5'>Retour à l'accueil</Button>
+      </Link>
+  </div>
+  );
+}
