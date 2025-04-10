95d6e64dd14b9df552f92905fd669e539ce26cdf
Christophe Goudet <goudetdata@gmail.com>
2025-04-10 23:41:54 +0200

    remove epty pipe

--- back/scripts/datasets/marches.py
+++ back/scripts/datasets/marches.py
@@ -92,7 +92,7 @@ class MarchesPublicsWorkflow(DatasetAggregator):
         interim_fn = raw_filename.parent / "interim.json"
         if not interim_fn.exists():
             return None
-        out = pd.read_json(interim_fn).rename(columns=COLUMNS_RENAMER).pipe()
+        out = pd.read_json(interim_fn).rename(columns=COLUMNS_RENAMER)
         object_columns = out.select_dtypes(include=["object"]).columns
         corrected = {c: out[c].astype("string").where(out[c].notnull()) for c in object_columns}
         return out.assign(**corrected)
