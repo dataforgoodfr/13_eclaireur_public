44b2b030935661215f42c2d2bc1e9211d42c1d2b
theocampbell <57679708+theocampbell@users.noreply.github.com>
2025-04-14 22:38:44 +0200

    move constants to separate file

---
+++ front/components/Map/constants.ts
@@ -0,0 +1,20 @@
+export const MAPTILER_API_KEY = process.env.NEXT_PUBLIC_MAPTILES_API_KEY;
+export const MAX_FEATURES_LOAD = 5000;
+export const DEFAULT_VIEW_STATE = {
+  longitude: 2.2137,
+  latitude: 46.2276,
+  zoom: 5,
+}
+
+export const BASE_MAP_STYLE = {
+  version: 8,
+  sources: {},
+  layers: [
+    {
+      id: 'background',
+      type: 'background',
+      paint: { 'background-color': '#f9f9f9' },
+    },
+  ],
+  glyphs: `https://api.maptiler.com/fonts/{fontstack}/{range}.pbf?key=${MAPTILER_API_KEY}`,
+};
