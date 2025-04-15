fc228ff880ea12f326f4e1894d277a25afc773c1
chloebarre <chlo.barre@gmail.com>
2025-04-15 13:27:59 +0200

    None

--- back/scripts/utils/dataframe_operation.py
+++ back/scripts/utils/dataframe_operation.py
@@ -274,9 +274,7 @@ def normalize_date(frame: pd.DataFrame, id_col: str) -> pd.DataFrame:
     if id_col not in frame.columns:
         return frame
     if frame[id_col].isnull().all():
-        return frame.assign(
-            **{id_col: pd.Series([pd.NaT] * len(frame), dtype="datetime64[ns, UTC]")}
-        )
+        return frame.assign(**{id_col: None})
 
     if str(frame[id_col].dtype) == "datetime64[ns, UTC]":
         dt = frame[id_col]
