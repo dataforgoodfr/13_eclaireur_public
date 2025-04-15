a4a532bce53ec14f34aeda115a89e6709f4cd9ac
GF <guillaume.fontanet@gmx.fr>
2025-04-15 12:36:32 +0200

    [BACK] MP enricher type identifiant du titulaire

--- back/scripts/enrichment/marches_enricher.py
+++ back/scripts/enrichment/marches_enricher.py
@@ -32,7 +32,11 @@ class MarchesPublicsEnricher(BaseEnricher):
         # Data analysts, please add your code here!
         marches, cpv_labels, *_ = inputs
 
-        marches = marches.pipe(cls.forme_prix_enrich).pipe(cls.type_prix_enrich)
+        marches = (
+            marches.pipe(cls.forme_prix_enrich)
+            .pipe(cls.type_prix_enrich)
+            .pipe(cls.type_identifiant_titulaire_enrich)
+        )
 
         # do stuff with sirene
         marches_pd = (
@@ -94,3 +98,62 @@ class MarchesPublicsEnricher(BaseEnricher):
             .rename({"typePrix": "type_prix"})
             .drop(["typesPrix", "TypePrix"])
         )
+
+    @staticmethod
+    def type_identifiant_titulaire_enrich(marches: pl.DataFrame) -> pl.DataFrame:
+        """
+        Normalize titulaire_typeIdentifiant column from titulaire_typeIdentifiant and titulaire_id.
+        """
+
+        # TODO : Il y a encore environ 600 titulaire_typeIdentifiant avec des titulaire_id non null qui sont null
+        SIRET_REGEX = r"^\d{14}$"  # 14 chiffres uniquement
+        SIREN_REGEX = r"^\d{9}$"  # 9 chiffres uniquement
+        TVA_REGEX = r"^[A-Z]{2}\d{9,12}$"  # Ex: GB123456789 ou FR12345678912
+
+        mapping = {
+            "HORS_UE": "HORS-UE",
+            "TVA_INTRACOMMUNAUTAIRE": "TVA",
+            "FRW": "FRWF",
+            "UE": "TVA",
+        }
+        marches = marches
+
+        marches = (
+            marches.with_columns(
+                pl.when(pl.col("titulaire_typeIdentifiant").is_not_null()).then(
+                    pl.col("titulaire_typeIdentifiant").replace_strict(
+                        mapping, default=pl.col("titulaire_typeIdentifiant")
+                    )
+                )
+            )
+            .with_columns(
+                pl.when(
+                    pl.col("titulaire_typeIdentifiant").is_null()
+                    & pl.col("titulaire_id").str.contains(SIRET_REGEX)
+                )
+                .then(pl.lit("SIRET"))
+                .otherwise(pl.col("titulaire_typeIdentifiant"))
+                .alias("titulaire_typeIdentifiant")
+            )
+            .with_columns(
+                pl.when(
+                    pl.col("titulaire_typeIdentifiant").is_null()
+                    & pl.col("titulaire_id").str.contains(SIREN_REGEX)
+                )
+                .then(pl.lit("SIREN"))
+                .otherwise(pl.col("titulaire_typeIdentifiant"))
+                .alias("titulaire_typeIdentifiant")
+            )
+            .with_columns(
+                pl.when(
+                    pl.col("titulaire_typeIdentifiant").is_null()
+                    & pl.col("titulaire_id").str.contains(TVA_REGEX)
+                )
+                .then(pl.lit("TVA"))
+                .otherwise(pl.col("titulaire_typeIdentifiant"))
+                .alias("titulaire_type_identifiant")
+            )
+            .drop("titulaire_typeIdentifiant")
+        )
+
+        return marches
