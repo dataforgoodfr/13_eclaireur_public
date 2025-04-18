f4a26f99550ef5975dda200873c177411b0ec4a9
Benjamin AUBRON <benjamin.aubron@outlook.com>
2025-04-18 15:12:53 +0200

    fetcher subventions working

--- front/app/community/[siren]/components/FicheSubventions/FicheSubventions.tsx
+++ front/app/community/[siren]/components/FicheSubventions/FicheSubventions.tsx
@@ -1,7 +1,6 @@
 import { NoData } from '@/app/community/[siren]/components/NoData';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { fetchSubventions } from '@/utils/fetchers/subventions/fetchSubventions-server';
-import { CommunityType } from '@/utils/types';
 
 // import Top10 from './Top10';
 // import Treemap from './Treemap';
@@ -9,51 +8,54 @@ import Trends from './Trends';
 
 async function getSubventions(siren: string) {
   // Ville de Rodez 211202023
-  // Ville de Lyon 200046977 
-  // Ville de Bordeaux 243300316 
-  const subventionsResults = await fetchSubventions({attribuant_siren : "200046977"});
+  // Métropole de Lyon 200046977
+  // Ville de Bordeaux 243300316
+  const subventionsResults = await fetchSubventions({ filters: { attribuant_siren: siren }, limit: 100 });
 
   return subventionsResults;
 }
 
 export async function FicheSubventions({ siren }: { siren: string }) {
-  const subventions = await getSubventions(siren);
-  console.log(subventions)
+  const subventions = await getSubventions("200046977");
+  console.log(siren);
+  console.log(subventions);
+  console.log(subventions.length);
 
   return (
     <>
-    <div className='mx-auto my-6 max-w-screen-2xl rounded-xl border p-6 shadow'>
-      <h2 className='pb-3 text-center text-2xl'>Subventions</h2>
-      {subventions.length > 0 ? (
-        <Tabs defaultValue='trends'>
-          <TabsList>
-            <TabsTrigger value='trends'>Évolution</TabsTrigger>
-            <TabsTrigger value='distribution'>Répartition</TabsTrigger>
-            <TabsTrigger value='compare'>Comparaison</TabsTrigger>
-            <TabsTrigger value='details'>Classement</TabsTrigger>
-          </TabsList>
-          <TabsContent value='trends'>
-            <Trends data={subventions}/>
-          </TabsContent>
-          <TabsContent value='distribution'>
-            <div className='flex h-[600px] w-full items-center justify-center bg-neutral-200'>
-              En construction
-            </div>
-          </TabsContent>
-          <TabsContent value='compare'>
-            <div className='flex h-[600px] w-full items-center justify-center bg-neutral-200'>
-              En construction
-            </div>
-          </TabsContent>
-          <TabsContent value='details'>
-            <div className='flex h-[600px] w-full items-center justify-center bg-neutral-200'>
-              En construction
-            </div>
-          </TabsContent>
-        </Tabs>
-      ) : (
-        <NoData />
-      )}
-    </div>    </>
-  )
+      <div className='mx-auto my-6 max-w-screen-2xl rounded-xl border p-6 shadow'>
+        <h2 className='pb-3 text-center text-2xl'>Subventions</h2>
+        {subventions.length > 0 ? (
+          <Tabs defaultValue='trends'>
+            <TabsList>
+              <TabsTrigger value='trends'>Évolution</TabsTrigger>
+              <TabsTrigger value='distribution'>Répartition</TabsTrigger>
+              <TabsTrigger value='compare'>Comparaison</TabsTrigger>
+              <TabsTrigger value='details'>Classement</TabsTrigger>
+            </TabsList>
+            <TabsContent value='trends'>
+              <Trends data={subventions} />
+            </TabsContent>
+            <TabsContent value='distribution'>
+              <div className='flex h-[600px] w-full items-center justify-center bg-neutral-200'>
+                En construction
+              </div>
+            </TabsContent>
+            <TabsContent value='compare'>
+              <div className='flex h-[600px] w-full items-center justify-center bg-neutral-200'>
+                En construction
+              </div>
+            </TabsContent>
+            <TabsContent value='details'>
+              <div className='flex h-[600px] w-full items-center justify-center bg-neutral-200'>
+                En construction
+              </div>
+            </TabsContent>
+          </Tabs>
+        ) : (
+          <NoData />
+        )}
+      </div>{' '}
+    </>
+  );
 }
