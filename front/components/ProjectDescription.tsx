19876fc1679a470e55cf6855132cdf66eb7e93f9
OctaveLT <ole_tullier@orange.fr>
2025-04-25 15:46:10 +0200

    fix: add space before percentage

--- front/components/ProjectDescription.tsx
+++ front/components/ProjectDescription.tsx
@@ -52,7 +52,7 @@ export default async function ProjectDescription() {
           <div className='grid grid-cols-1 place-content-center gap-10 pb-20 xl:grid-cols-2'>
             <ChiffreCle
               className='rotate-[3deg] shadow-[3px_3px_0px_0px_rgba(0,0,0,0.1)]'
-              value={`${kpis.publishedSubventionsPercentage}%`}
+              value={`${kpis.publishedSubventionsPercentage} %`}
               description='des subventions en montant sont publiées.'
             />
             <ChiffreCle
@@ -64,7 +64,7 @@ export default async function ProjectDescription() {
               description='de budget total de subventions dans les collectivités.'
             />
             <ChiffreCle
-              value={`${kpis.communitiesGoodScoresPercentage}%`}
+              value={`${kpis.communitiesGoodScoresPercentage} %`}
               description='des collectivités ont un score A ou B.'
             />
           </div>
