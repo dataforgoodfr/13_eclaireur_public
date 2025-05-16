b963268b37c224a37324e129c51601ca62dc1b27
Antoine Quesnel (perso) <antoine.quesnel@hotmail.fr>
2025-05-16 17:15:46 +0200

    [FRONT] Fiche Collectivité - Maj filtre année graphs feat: sélectionner par défaut l'année la plus récente avec des données feat: afficher toutes les années depuis 2018

--- front/app/community/[siren]/components/YearSelector.tsx
+++ front/app/community/[siren]/components/YearSelector.tsx
@@ -7,21 +7,23 @@ import {
 } from '@/components/ui/select';
 
 import { YearOption } from '../types/interface';
+import { getAllYearsFrom2018ToCurrent } from '@/utils/utils';
 
 type YearSelectorProps = {
-  years: number[];
+  defaultValue: number | 'All';
   onSelect: (option: YearOption) => void;
 };
 
-export default function YearSelector({ years, onSelect }: YearSelectorProps) {
+export default function YearSelector({ defaultValue, onSelect }: YearSelectorProps) {
   return (
-    <Select onValueChange={(value) => onSelect(value === 'All' ? 'All' : parseInt(value))}>
+    <Select onValueChange={(value) => onSelect(value === 'All' ? 'All' : parseInt(value))}
+      defaultValue={defaultValue.toString()}>
       <SelectTrigger className='w-[100px]'>
         <SelectValue placeholder='Tout voir' />
       </SelectTrigger>
       <SelectContent>
         <SelectItem value='All'>Tout voir</SelectItem>
-        {years.map((year, index) => (
+        {getAllYearsFrom2018ToCurrent().map((year, index) => (
           <SelectItem key={index} value={String(year)}>
             {year}
           </SelectItem>
