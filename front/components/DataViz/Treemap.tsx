1e2ae7a4ae4a3485018286fc334902c6cf349d56
theocampbell <57679708+theocampbell@users.noreply.github.com>
2025-06-06 13:23:31 +0200

    Revert "fix remaining lint errors"
    This reverts commit f2cd33d4a399725a949f9a311d8f09f7c59f221e.

--- front/components/DataViz/Treemap.tsx
+++ front/components/DataViz/Treemap.tsx
@@ -42,7 +42,7 @@ function generateColorMap(names: string[]): Record<string, string> {
   return colorMap;
 }
 
-type TreemapProps = { data: TreeData; isZoomActive: boolean; handleClick: (value: number) => void };
+type TreemapProps = { data: TreeData; isZoomActive: boolean; handleClick: Function };
 
 export default function Treemap({ data, isZoomActive, handleClick }: TreemapProps) {
   const [tooltip, setTooltip] = useState<TooltipProps>({
