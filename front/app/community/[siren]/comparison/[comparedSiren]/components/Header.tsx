9b155c362bec7536826fdf90970f944487980ee4
Antoine Quesnel (perso) <antoine.quesnel@hotmail.fr>
2025-07-04 18:20:12 +0200

    feat: ajouter la recherche de collectivités dans la page de comparaison

--- front/app/community/[siren]/comparison/[comparedSiren]/components/Header.tsx
+++ front/app/community/[siren]/comparison/[comparedSiren]/components/Header.tsx
@@ -1,6 +1,9 @@
 import { Community } from '@/app/models/community';
 
+import { FicheComparisonInput } from '../../../components/FicheHeader/FicheComparisonInput';
 import GoBack from '../../../components/GoBack';
+import { SeparatorHorizontal } from 'lucide-react';
+import { Separator } from '@radix-ui/react-select';
 
 type FicheHeaderProps = {
   community: Community;
@@ -13,15 +16,16 @@ export function Header({ community, community2 }: FicheHeaderProps) {
   // TODO - get and show postal code
   const title = `Comparaison entre ${community.nom} et ${community2.nom}`;
   // TODO - get and show last update date
-  const lastUpdateText = `Dernière mise à jour le XX/XX/XX`;
 
   return (
     <div className='flex w-full flex-col justify-stretch gap-6 bg-gray-200 p-6 md:flex-row'>
       <GoBack />
       <div className='flex-1 text-center'>
         <p className='text-xl font-bold'>{title}</p>
-        <p className='mb-4 text-gray-500'>{lastUpdateText}</p>
-        <p>{descriptionText}</p>
+        <p className='mt-2'>{descriptionText}</p>
+        <div className='mx-auto mt-4 w-1/2 text-sm'>
+          <FicheComparisonInput community={community} />
+        </div>
       </div>
     </div>
   );
