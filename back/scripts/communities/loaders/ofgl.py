0668a5cca08d6c34bdaae342fa9add4d58ac1389
jsm-shams <207422542+jsm-shams@users.noreply.github.com>
2025-07-18 10:31:25 +0200

    chore(ofgl): add guard against null df in _read_parse_file method

--- back/scripts/communities/loaders/ofgl.py
+++ back/scripts/communities/loaders/ofgl.py
@@ -66,28 +66,30 @@ class OfglLoader(DatasetAggregator):
         opts = {"columns": READ_COLUMNS.keys(), "dtype": COM_CSV_DTYPES}
 
         loader = BaseLoader.loader_factory(raw_filename, **opts)
-        df = (
-            loader.load()
-            .rename(columns={k: v for k, v in READ_COLUMNS.items() if v})
-            .pipe(normalize_column_names)
-            .assign(
-                type=file_metadata.code,
-                outre_mer=lambda df: (df["outre_mer"] == "Oui").fillna(False),
+        df = loader.load()
+        if df :
+            df = (
+                df
+                .rename(columns={k: v for k, v in READ_COLUMNS.items() if v})
+                .pipe(normalize_column_names)
+                .assign(
+                    type=file_metadata.code,
+                    outre_mer=lambda df: (df["outre_mer"] == "Oui").fillna(False),
+                )
             )
-        )
 
-        insee_col = self.INSEE_COL.get(file_metadata.code)
-        if insee_col:
-            df = df.assign(code_insee=lambda df: df[insee_col])
+            insee_col = self.INSEE_COL.get(file_metadata.code)
+            if insee_col:
+                df = df.assign(code_insee=lambda df: df[insee_col]) 
 
-        df = (
-            df.sort_values("exercice", ascending=False)
-            .drop_duplicates(subset=["siren"], keep="first")
-            .pipe(normalize_identifiant, id_col="siren", format=IdentifierFormat.SIREN)
-        )
+            df = (
+                df.sort_values("exercice", ascending=False)
+                .drop_duplicates(subset=["siren"], keep="first")
+                .pipe(normalize_identifiant, id_col="siren", format=IdentifierFormat.SIREN)
+            )
 
-        self.columns = pd.concat(
-            [self.columns, pd.DataFrame({file_metadata.code: 1}, index=df.columns)], axis=1
-        )
-        self.columns.to_csv(self.data_folder / "columns.csv")
+            self.columns = pd.concat(
+                [self.columns, pd.DataFrame({file_metadata.code: 1}, index=df.columns)], axis=1
+            )
+            self.columns.to_csv(self.data_folder / "columns.csv")
         return df
