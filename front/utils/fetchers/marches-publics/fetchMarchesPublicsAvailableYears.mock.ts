4355f0ee4aaa325c59b0e5e4e5b7e2bdb6c5db81
Thomas PEDOT <thomas.pedot@gmail.com>
2025-07-26 15:48:15 +0200

    fix: path

--- front/utils/fetchers/marches-publics/fetchMarchesPublicsAvailableYears.mock.ts
+++
@@ -1,4 +0,0 @@
-import { fn } from '@storybook/test';
-
-export * from './fetchMarchesPublicsAvailableYears';
-export const fetchMarchesPublicsAvailableYears = fn().mockName('fetchMarchesPublicsAvailableYears');
