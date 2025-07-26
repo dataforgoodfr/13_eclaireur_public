cdcf9160e28ab55060c7c31df85246c61deb7f91
Thomas PEDOT <thomas.pedot@gmail.com>
2025-07-26 18:34:17 +0200

    feat: Update FicheIdentite mock data

--- front/app/community/[siren]/components/FicheIdentite/FicheIdentite.tsx
+++ front/app/community/[siren]/components/FicheIdentite/FicheIdentite.tsx
@@ -1,30 +1,37 @@
 import type { Community } from '#app/models/community';
-
+import { CircleX, FileText } from 'lucide-react';
 import { CommunityDetails } from '../CommunityDetails';
+
+
 import { FicheCard } from '../FicheCard';
 import NeighboursMap from '../NeighboursMap/NeighboursMap';
 
-type FicheIdentiteProps = {
-  community: Community;
-};
 
-const FicheIndentiteEnTete = () => {
+const FicheIndentiteEnTete = ({ community }: { community: Community }) => {
   return (
     <div className='flex flex-col sm:flex-row justify-between items-start sm:items-center'>
       <h2 className='text-3xl md:text-4xl font-extrabold text-primary'>
         Informations générales
       </h2>
-      <span className='text-xs sm:text-sm px-3 py-1 mt-2 sm:mt-0 rounded-full bg-lime-200 text-primary font-bold'>
-        Soumise à l’obligation Loi République Numérique
-      </span>
+      {community.should_publish ? (
+        <span className='text-xs sm:text-sm px-3 py-1 mt-2 sm:mt-0 rounded-full bg-lime-200 text-primary font-bold flex items-center gap-2'>
+          <FileText size={12} />
+          Soumise à l’obligation Loi République Numérique
+        </span>
+      ) : (
+        <span className='text-xs sm:text-sm px-3 py-1 mt-2 sm:mt-0 rounded-full bg-red-200 text-primary font-bold  flex items-center gap-2'>
+          <CircleX size={12} />
+          Non soumise à l’obligation Loi République Numérique
+        </span>
+      )}
     </div>
   );
 };
 
-export function FicheIdentite({ community }: FicheIdentiteProps) {
+export function FicheIdentite({ community }: { community: Community }) {
 
   return (
-    <FicheCard header={<FicheIndentiteEnTete />}>
+    <FicheCard header={<FicheIndentiteEnTete community={community} />}>
       <div className="mb-10 flex flex-col-reverse md:flex-row w-full gap-6">
         <section className="w-full md:w-1/3 flex flex-col gap-6">
           <CommunityDetails community={community} />
