4aac5c8ec9a7c2c5ba08e6a71e19eebae030f7c3
Thomas PEDOT <thomas.pedot@gmail.com>
2025-07-26 17:57:04 +0200

    add rounded corner to transparency score

--- front/components/TransparencyScore/TransparencyScore.tsx
+++ front/components/TransparencyScore/TransparencyScore.tsx
@@ -8,38 +8,58 @@ import {
 import { cn } from '#utils/utils';
 import { ClassNameValue } from 'tailwind-merge';
 
-const SQUARE_WIDTH = 60;
+const SQUARE_SIZE = 60;
+const CORNER_RADIUS = 12;
 const ACTIVE_SCORE_SCALE = 1.2;
+const GAP = 10;
+
+const scoreValues = Object.values(TransparencyScore);
 
 const SVG_CONFIG = {
-  viewBoxWidth: SQUARE_WIDTH * 5 + 40,
-  viewBoxHeight: SQUARE_WIDTH * 2,
+  viewBoxWidth: scoreValues.length * (SQUARE_SIZE + GAP),
+  viewBoxHeight: SQUARE_SIZE * 2,
   margin: 20,
 };
 
 type ScoreTileProps = {
   score: TransparencyScore;
-  // x: number;
-  size?: number;
   rectangleClassName?: ClassNameValue;
+  size?: number;
 } & SVGProps<SVGGElement>;
+
+function getCustomRoundedRectPath(width: number, height: number, r: number) {
+  return `
+    M 0,0
+    L ${width},0
+    L ${width},${height - r}
+    Q ${width},${height} ${width - r},${height}
+    L 0,${height}
+    L 0,${r}
+    Q 0,0 ${r},0
+    Z
+  `;
+}
 
 function ScoreTile({
   score,
-  // x,
-  size = SQUARE_WIDTH,
   rectangleClassName,
+  size = SQUARE_SIZE,
   ...restProps
 }: ScoreTileProps) {
   return (
-    <g key={score} {...restProps}>
-      <rect
-        width={size}
-        height={size}
+    <g {...restProps}>
+      <path
+        d={getCustomRoundedRectPath(size, size, CORNER_RADIUS)}
+        className={cn(rectangleClassName)}
         strokeWidth={1}
-        className={cn('fill-transparent stroke-slate-400', rectangleClassName)}
       />
-      <text x={SQUARE_WIDTH * 0.5} y={SQUARE_WIDTH / 2 + 5} textAnchor='middle'>
+      <text
+        x={size / 2}
+        y={size / 2 + 5 / 2}
+        textAnchor='middle'
+        dominantBaseline='middle'
+        className='fill-blue-900 font-bold'
+      >
         {score}
       </text>
     </g>
@@ -51,56 +71,49 @@ type TransparencyScoreBarProps = {
 };
 
 export function TransparencyScoreBar({ score: activeScore }: TransparencyScoreBarProps) {
-  function isActiveScore(scoreValue: TransparencyScore) {
-    return scoreValue === activeScore;
-  }
-
-  const scoreValues = Object.values(TransparencyScore);
   const activeScoreIndex =
-    activeScore === null ? 2 : scoreValues.findIndex((scorevalue) => scorevalue === activeScore);
+    activeScore === null ? 2 : scoreValues.findIndex((s) => s === activeScore);
 
   const translateDueToScaleFactor = -5;
 
   return (
     <svg
-      width={SVG_CONFIG.viewBoxWidth}
+      width={SVG_CONFIG.viewBoxWidth + 2 * SVG_CONFIG.margin}
       height={SVG_CONFIG.viewBoxHeight}
-      viewBox={`0 0 ${SVG_CONFIG.viewBoxWidth} ${SVG_CONFIG.viewBoxHeight}`}
+      viewBox={`0 0 ${SVG_CONFIG.viewBoxWidth + 2 * SVG_CONFIG.margin} ${SVG_CONFIG.viewBoxHeight}`}
     >
       <g transform={`translate(${SVG_CONFIG.margin}, ${SVG_CONFIG.margin})`}>
         {scoreValues.map((scoreValue, i) => {
-          if (isActiveScore(scoreValue)) return null;
+          if (scoreValue === activeScore) return null;
 
           return (
-            <g key={scoreValue}>
-              <ScoreTile
-                score={scoreValue}
-                x={SQUARE_WIDTH * i}
-                transform={`translate(${SQUARE_WIDTH * i}, 0)`}
-              />
-            </g>
+            <ScoreTile
+              key={scoreValue}
+              score={scoreValue}
+              transform={`translate(${i * (SQUARE_SIZE + GAP)}, 0)`}
+              rectangleClassName='fill-gray-200 rounded-none rounded-br-2xl rounded-tl-2xl'
+            />
           );
         })}
+
         {activeScore !== null && (
-          <g className='font-bold'>
-            <ScoreTile
-              score={activeScore}
-              x={SQUARE_WIDTH * activeScoreIndex}
-              transform={`scale(${ACTIVE_SCORE_SCALE}) translate(${(SQUARE_WIDTH * activeScoreIndex) / ACTIVE_SCORE_SCALE + translateDueToScaleFactor}, ${translateDueToScaleFactor})`}
-              rectangleClassName='fill-slate-200'
-            />
-          </g>
+          <ScoreTile
+            score={activeScore}
+            transform={`scale(${ACTIVE_SCORE_SCALE}) translate(${(activeScoreIndex * (SQUARE_SIZE + GAP)) / ACTIVE_SCORE_SCALE + translateDueToScaleFactor
+              }, ${translateDueToScaleFactor})`}
+            rectangleClassName='fill-lime-500 rounded-none rounded-br-2xl rounded-tl-2xl'
+          />
         )}
       </g>
+
       <g transform={`translate(${SVG_CONFIG.margin}, ${SVG_CONFIG.margin})`}>
         <text
-          x={SQUARE_WIDTH * (activeScoreIndex + 0.5)}
-          y={SQUARE_WIDTH + 25}
+          x={activeScoreIndex * (SQUARE_SIZE + GAP) + SQUARE_SIZE / 2}
+          y={SQUARE_SIZE + 25}
           textAnchor='middle'
-          className='font-bold'
+          className='font-bold fill-blue-900 text-lg'
         >
-          {activeScore !== null && SCORE_TO_ADJECTIF[activeScore]}
-          {activeScore === null && SCORE_NON_DISPONIBLE}
+          {activeScore !== null ? SCORE_TO_ADJECTIF[activeScore] : SCORE_NON_DISPONIBLE}
         </text>
       </g>
     </svg>
