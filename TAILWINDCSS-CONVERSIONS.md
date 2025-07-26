6041a778c465f316986f1a33240e79680ca46ca6
Thomas PEDOT <thomas.pedot@gmail.com>
2025-07-26 17:57:04 +0200

    feat: add Tailwind CSS style guide and update community details
    - adds new style Kanit
    - update fiche identity

---
+++ TAILWINDCSS-CONVERSIONS.md
@@ -0,0 +1,14 @@
+# Style map
+
+| Style            | Desktop Size | Desktop Height | Mobile Size | Mobile Height | Tailwind CSS Classes     |
+| ---------------- | ------------ | -------------- | ----------- | ------------- | ------------------------ |
+| H1               | 44px         | 56px           | 36px        | 40px          | `text-3xl md:text-4xl`   |
+| H2               | 40px         | 44px           | 32px        | 38px          | `text-3xl md:text-4xl`   |
+| H3               | 32px         | 36px           | 28px        | 32px          | `text-2xl md:text-3xl`   |
+| H4               | 24px         | 28px           | 22px        | 28px          | `text-xl md:text-2xl`    |
+| Tag              | -            | -              | 14px        | auto          | `text-sm`                |
+| Texte intro      | 22px         | 28px           | 18px        | auto          | `text-lg md:text-xl`     |
+| Texte            | 18px         | 24px           | 18px        | 24px          | `text-lg`                |
+| Texte-Light      | 18px         | 24px           | 18px        | 24px          | `text-lg font-light`     |
+| Texte-Bold       | 18px         | 24px           | 18px        | 24px          | `text-lg font-bold`      |
+| Texte-Extra Bold | 18px         | 24px           | 18px        | 24px          | `text-lg font-extrabold` |
--- front/app/community/[siren]/components/FicheIdentite/FicheIdentite.tsx
+++ front/app/community/[siren]/components/FicheIdentite/FicheIdentite.tsx
@@ -1,29 +1,38 @@
-import { Community } from '#app/models/community';
-import { TransparencyScore } from '#components/TransparencyScore/constants';
+import type { Community } from '#app/models/community';
 
 import { CommunityDetails } from '../CommunityDetails';
 import { FicheCard } from '../FicheCard';
 import NeighboursMap from '../NeighboursMap/NeighboursMap';
-import { TransparencyScoreWithTrend } from '../TransparencyScore/TransparencyScore';
 
 type FicheIdentiteProps = {
   community: Community;
 };
 
+const FicheIndentiteEnTete = () => {
+  return (
+    <div className='flex flex-col sm:flex-row justify-between items-start sm:items-center'>
+      <h2 className='text-3xl md:text-4xl font-extrabold text-primary'>
+        Informations générales
+      </h2>
+      <span className='text-xs sm:text-sm px-3 py-1 mt-2 sm:mt-0 rounded-full bg-lime-200 text-primary font-bold'>
+        Soumise à l’obligation Loi République Numérique
+      </span>
+    </div>
+  );
+};
+
 export function FicheIdentite({ community }: FicheIdentiteProps) {
-  // TODO - get and add the last update date
-  // const lastUpdateText = `Derniere mise a jour`;
-  // TODO - retrieve scores
-  const score = TransparencyScore.B;
-  const trend = 1;
 
   return (
-    <FicheCard>
+    <FicheCard header={<FicheIndentiteEnTete />}>
       <div className='mb-10 flex flex-col items-center justify-between gap-6 md:flex-row md:items-start'>
-        <CommunityDetails community={community} />
-        <NeighboursMap community={community} />
+        <section className='w-full flex flex-col gap-6'>
+          <CommunityDetails community={community} />
+        </section>
+        <section className='w-full flex flex-col gap-6'>
+          <NeighboursMap community={community} />
+        </section>
       </div>
-      <TransparencyScoreWithTrend score={score} trend={trend} />
     </FicheCard>
   );
 }
