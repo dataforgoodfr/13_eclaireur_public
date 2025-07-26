45737da3842ad89edd7cb4447b31836baa1f344f
Jean-Baptiste Delafosse <jean-baptiste@lumapps.com>
2025-07-26 15:03:35 +0200

    chore: final validation steps on ofg inputs

--- back/scripts/entities/ofgl.py
+++ back/scripts/entities/ofgl.py
@@ -1,6 +1,5 @@
 import pandera.polars as pa
-import polars as pl
-from pandera.typing.polars import Series
+from pandera import extensions
 
 
 def is_valid_siren(siren: str) -> bool:
@@ -20,88 +19,46 @@ def is_valid_siren(siren: str) -> bool:
     return total % 10 == 0
 
 
-class Ofgl2023RecordDataframe(pa.DataFrameModel):
-    exer: str = pa.Field(
-        nullable=True, description="Exercice budgétaire", title="Exercice", alias="Exercice"
-    )
-    outre_mer: str = pa.Field(
-        nullable=True,
-        description="Appartenance de la collectivité à l'outre-mer : oui/non",
-        title="Outre-mer",
-        alias="Outre-mer",
-    )
-    reg_code: str = pa.Field(
-        nullable=True,
-        description="Numéro Insee 2023 de la région ou assimilé",
-        title="Code Insee 2023 Région",
-        alias="Code Insee 2023 Région",
-    )
-    categ: str = pa.Field(
-        nullable=True,
-        description="Catégorie de collectivité : Commune pour commune, PARIS pour la ville de Paris",
-        title="Catégorie",
-        alias="Catégorie",
-    )
-    siren: str = pa.Field(
-        nullable=True,
-        description="Numéro Siren de la commune",
-        title="Code Siren Collectivité",
-        alias="Code Siren Collectivité",
-    )
-    ptot: int = pa.Field(
-        nullable=True,
-        coerce=True,
-        description="Population Insee totale au 1er janvier de l'exercice",
-        title="Population totale",
-        alias="Population totale",
-    )
-
-    @pa.check("Exercice")
-    @classmethod
-    def check_exer_format(cls, series: pl.Series) -> pl.Series:
-        return series.str.contains(r"^\d{4}$")
-
-    @pa.check("Outre-mer")
-    @classmethod
-    def check_outre_mer_values(cls, series: pl.Series) -> pl.Series:
-        return series.is_in(["Oui", "Non"])
-
-    @pa.check("Code Insee 2023 Région")
-    @classmethod
-    def check_reg_code_format(cls, series: pl.Series) -> pl.Series:
-        return series.str.contains(r"^\d{2,3}$")
-
-    @pa.check("Code Siren Collectivité")
-    @classmethod
-    def check_siren_validity(cls, series: pl.Series) -> pl.Series:
-        return series.map_elements(is_valid_siren, return_dtype=pl.Boolean)
-
-    @pa.check("Population totale")
-    @classmethod
-    def check_ptot_positive(cls, series: pl.Series) -> pl.Series:
-        return series >= 0
-
-    class Config:
-        strict = "filter"
-        coerce = True
+@extensions.register_check_method(check_type="element_wise")
+def siren_check_validity(siren: str) -> bool:
+    """
+    Vérifie la validité du numéro SIREN.
+    """
+    # Appelle la fonction is_valid_siren définie ci-dessus
+    return is_valid_siren(siren)
 
 
 class Ofgl2024RecordDataframe(pa.DataFrameModel):
     exer: str = pa.Field(
-        nullable=True, description="Exercice budgétaire", title="Exercice", alias="Exercice"
+        nullable=True,
+        description="Exercice budgétaire",
+        title="Exercice",
+        alias="Exercice",
+        str_matches=r"^\d{4}$",
     )
     outre_mer: str = pa.Field(
         nullable=True,
         description="Appartenance de la collectivité à l'outre-mer : oui/non",
         title="Outre-mer",
         alias="Outre-mer",
+        isin=["Oui", "Non"],
     )
     reg_code: str = pa.Field(
         nullable=True,
-        description="Numéro Insee 2024 de la région ou assimilé",
+        description="Numéro Insee 2024 de la région ou assimilé. Attention : Peut contenir deux codes régions séparés par une virgule.",
         title="Code Insee 2024 Région",
         alias="Code Insee 2024 Région",
+        str_matches=r"^\d{1,2}(,\d{1,2})*$",
     )
+
+    dep_code: str | None = pa.Field(
+        nullable=True,
+        description="Numéro Insee 2024 du département ou assimilé. Attention : Peut contenir plusieurs codes départements séparés par une virgule.",
+        title="Code Insee 2024 Département",
+        alias="Code Insee 2024 Département",
+        str_matches=r"^(0[1-9]|[1-8]\d|9[0-5]|97[1-46]|2[AB]|67A|691)(,(0[1-9]|[1-8]\d|9[0-5]|97[1-46]|2[AB]|67A|691))*$",
+    )
+
     categ: str = pa.Field(
         nullable=True,
         description="Catégorie de collectivité : Commune pour commune, PARIS pour la ville de Paris",
@@ -113,40 +70,25 @@ class Ofgl2024RecordDataframe(pa.DataFrameModel):
         description="Numéro Siren de la commune",
         title="Code Siren Collectivité",
         alias="Code Siren Collectivité",
+        siren_check_validity=True,
     )
+
     ptot: int = pa.Field(
         nullable=True,
         coerce=True,
         description="Population Insee totale au 1er janvier de l'exercice",
         title="Population totale",
         alias="Population totale",
+        ge=0,
     )
 
-    @pa.check("Exercice")
-    @classmethod
-    def check_exer_format(cls, series: pl.Series) -> pl.Series:
-        return series.str.contains(r"^\d{4}$")
-
-    @pa.check("Outre-mer")
-    @classmethod
-    def check_outre_mer_values(cls, series: pl.Series) -> pl.Series:
-        return series.is_in(["Oui", "Non"])
-
-    @pa.check("Code Insee 2024 Région")
-    @classmethod
-    def check_reg_code_format(cls, series: pl.Series) -> pl.Series:
-        return series.str.contains(r"^\d{2,3}$")
-
-    @pa.check("Code Siren Collectivité")
-    @classmethod
-    def check_siren_validity(cls, series: pl.Series) -> pl.Series:
-        return series.map_elements(is_valid_siren, return_dtype=pl.Boolean)
-
-    @pa.check("Population totale")
-    @classmethod
-    def check_ptot_positive(cls, series: pl.Series) -> pl.Series:
-        return series >= 0
+    insee: str | None = pa.Field(
+        nullable=True,
+        description="Code Insee de la commune",
+        title="Code Insee Collectivité",
+        alias="Code Insee Collectivité",
+    )
 
     class Config:
-        strict = "filter"
+        strict = True
         coerce = True
