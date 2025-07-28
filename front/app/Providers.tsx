10178eafd3bd580baf9977943d942090f601d555
Thomas PEDOT <thomas.pedot@gmail.com>
2025-07-28 11:48:25 +0200

    refactor: standardise imports

--- front/app/Providers.tsx
+++ front/app/Providers.tsx
@@ -2,15 +2,16 @@
 
 import { PropsWithChildren } from 'react';
 
-import { NuqsAdapter } from "nuqs/adapters/next/app";
-
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { NuqsAdapter } from 'nuqs/adapters/next/app';
 
 type ProvidersProps = PropsWithChildren;
 
 export default function Providers({ children }: ProvidersProps) {
   const queryClient = new QueryClient();
-  return <NuqsAdapter>
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-  </NuqsAdapter>
+  return (
+    <NuqsAdapter>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </NuqsAdapter>
+  );
 }
