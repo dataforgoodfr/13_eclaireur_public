93ecc5b36d1fea500272f1613efdf29615223b53
theocampbell <57679708+theocampbell@users.noreply.github.com>
2025-07-28 12:03:59 +0200

    FRONT: fix - remove base URL and replace with window.location.origin (#309)

--- front/utils/fetchers/marches-publics/fetchMarchesPublicsYearlyAmounts-client.ts
+++ front/utils/fetchers/marches-publics/fetchMarchesPublicsYearlyAmounts-client.ts
@@ -1,6 +1,5 @@
 import { YearlyAmount } from '#app/models/graphs';
 
-const baseURL = process.env.NEXT_PUBLIC_BASE_URL;
 function getAPIRoute(communitySiren: string) {
   return `/api/communities/${communitySiren}/marches_publics/yearly_amounts`;
 }
@@ -11,13 +10,13 @@ function getAPIRoute(communitySiren: string) {
 export async function fetchMarchesPublicsYearlyAmounts(
   communitySiren: string,
 ): Promise<YearlyAmount[]> {
-  const url = new URL(getAPIRoute(communitySiren), baseURL);
+  const url = new URL(getAPIRoute(communitySiren), window.location.origin);
 
-  const res = await fetch(url.toString(), { method: 'get' });
+  const res = await fetch(url.toString(), { method: 'GET' });
 
   if (!res.ok) {
     throw new Error('Failed to fetch marches publics yearly amounts with siren ' + communitySiren);
   }
 
-  return (await res.json()) as Promise<YearlyAmount[]>;
+  return await res.json();
 }
