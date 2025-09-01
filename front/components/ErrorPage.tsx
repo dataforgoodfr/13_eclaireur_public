a2badfafa9e3c2145a7e3d8f98c77daf413a1b72
Benjamin AUBRON <59196884+benjamin-aubron@users.noreply.github.com>
2025-09-01 19:01:30 +0200

    [FRONT] : Refactor - Error & NotFound (#406)

--- front/components/ErrorPage.tsx
+++ front/components/ErrorPage.tsx
@@ -6,17 +6,32 @@ import { Button } from '#components/ui/button';
 type ErrorPageProps = {
   title: string;
   description: string;
+  errorType: string;
 };
 
-export default function ErrorPage({ title, description }: ErrorPageProps) {
+export default function ErrorPage({ title, description, errorType }: ErrorPageProps) {
   return (
-    <div className='grid h-[500px] place-content-center justify-items-center bg-white px-4'>
-      <Image src='/error.svg' width={250} height={250} alt='' className='' />
-      <h1 className='text-xl text-gray-700'>{title}</h1>
-      <p className='text-gray-800'>{description}</p>
+    <div className='flex min-h-screen flex-col items-center bg-primary px-4 pb-20 pt-28 md:pt-40'>
+      <div>
+        <h2 className='text-center text-3xl text-white md:text-5xl'>Ouuuups ! </h2>
+        <h1 className='text-center text-3xl text-white md:text-5xl'>{title}</h1>
+      </div>
+      <Image
+        src={`${errorType === '404' ? '/eclaireur/404.png' : '/eclaireur/500.png'}`}
+        width={430}
+        height={100}
+        alt="Image d'erreur"
+        className='w-[280px] md:w-[430px]'
+      />
+      <p className='w-[300px] text-center text-xl text-white md:w-[600px] md:text-2xl'>
+        {description}
+      </p>
       <Link href={'/'}>
-        <Button variant='link' className='mt-5'>
-          Retour à l'accueil
+        <Button
+          variant='action'
+          className='mt-8 bg-[#fbf79f] text-primary drop-shadow-[0_0_30px_rgba(255,221,51,0.8)] hover:bg-[#fbf79f]/80'
+        >
+          Revenir à la lumière
         </Button>
       </Link>
     </div>
