35701b41291bf56c206034c2bfa8be2501e8cd89
chloebarre <chlo.barre@gmail.com>
2025-09-03 18:56:32 +0200

    Met to GRP

--- back/scripts/enrichment/communities_enricher.py
+++ back/scripts/enrichment/communities_enricher.py
@@ -57,6 +57,8 @@ class CommunitiesEnricher(BaseEnricher):
             ),
             on="siren",
             how="left",
+        ).with_columns(
+            pl.when(pl.col("type") == "MET").then("GRP").otherwise(pl.col("type")).alias("type")
         )
 
     @classmethod
