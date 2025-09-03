d0ab071e5f9a6a39ecdf04b02a268f81461481e1
chloebarre <chlo.barre@gmail.com>
2025-09-03 19:04:27 +0200

    correction

--- back/scripts/enrichment/communities_enricher.py
+++ back/scripts/enrichment/communities_enricher.py
@@ -58,7 +58,10 @@ class CommunitiesEnricher(BaseEnricher):
             on="siren",
             how="left",
         ).with_columns(
-            pl.when(pl.col("type") == "MET").then("GRP").otherwise(pl.col("type")).alias("type")
+            pl.when(pl.col("type") == "MET")
+            .then(pl.lit("GRP"))
+            .otherwise(pl.col("type"))
+            .alias("type")
         )
 
     @classmethod
