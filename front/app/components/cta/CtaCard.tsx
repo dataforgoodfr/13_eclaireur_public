73dec76101085941f8cbea2b5c18d95c81962909
Quentin <quentin@bloctobuild.com>
2025-09-05 17:57:58 +0200

    Contexte page

--- front/app/components/cta/CtaCard.tsx
+++ front/app/components/cta/CtaCard.tsx
@@ -27,17 +27,19 @@ export default function CtaCard({
   return (
     <Link
       href={href}
-      className={`box-border flex h-full w-full flex-col justify-between gap-4 rounded-br-xl rounded-tl-xl p-4 shadow-md ${colorClassName} transition-all duration-300 hover:translate-y-[-10px]`}
+      className={`box-border flex h-full w-full flex-col justify-between gap-4 rounded-br-xl rounded-tl-xl shadow-md ${colorClassName} transition-all duration-300 hover:translate-y-[-10px]`}
     >
-      <div className='flex flex-col'>
-        <Image src={picto} alt={title} className='w-6 pb-2' width={36} height={36} />
-        <h3 className={`${isCardBig ? 'mb-4' : ''}`}>{title}</h3>
-        <p className={isCardBig ? 'mb-4 font-bold' : ''}>{caption}</p>
-        {children}
-      </div>
-      <div className='flex justify-between'>
-        <span className='font-bold'>{buttonText}</span>
-        <ArrowRight className='transition-transform duration-300 group-hover:translate-x-1' />
+      <div className='p-5'>
+        <div className='flex flex-col'>
+          <Image src={picto} alt={title} className='w-6 pb-2' width={36} height={36} />
+          <h3 className={`${isCardBig ? 'mb-4' : ''}`}>{title}</h3>
+          <p className={isCardBig ? 'mb-4 font-bold' : ''}>{caption}</p>
+          {children}
+        </div>
+        <div className='flex justify-between'>
+          <span className='font-bold'>{buttonText}</span>
+          <ArrowRight className='transition-transform duration-300 group-hover:translate-x-1' />
+        </div>
       </div>
     </Link>
   );
