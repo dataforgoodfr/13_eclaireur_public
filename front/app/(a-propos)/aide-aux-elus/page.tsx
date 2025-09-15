c11eeba5e1a05e797db5a2b504ffe40bee474160
Benjamin AUBRON <benjamin.aubron@outlook.com>
2025-09-15 10:29:42 +0200

    help page - done

--- front/app/(a-propos)/aide-aux-elus/page.tsx
+++ front/app/(a-propos)/aide-aux-elus/page.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from 'next';
+import Image from 'next/image';
+import Link from 'next/link';
 
 import { SectionHeader } from '#app/components/SectionHeader';
 import {
@@ -54,51 +56,182 @@ export default function Page() {
           </ul>
         </div>
         <h2>Comment concrètement ouvrir ses données ?</h2>
+        <div>
+          <p>
+            Les données doivent être publiées sur la plateforme gouvernementale prévue à cet effet{' '}
+            <Link
+              href='https://data.gouv.fr'
+              target='_blank'
+              className='font-medium hover:underline'
+            >
+              data.gouv.fr
+            </Link>
+          </p>
+          <p>
+            Le guide {' '}
+            <Link
+              href='https://www.data.gouv.fr/pages/onboarding/producteurs/'
+              target='_blank'
+              className='font-medium hover:underline'
+            >
+              "Pourquoi et comment ouvrir vos données&nbsp;?"
+            </Link>
+            a été mis à disposition par le gouvernement pour faciliter la démarche de publication.
+          </p>
+          <p>
+            Le mini site{' '}
+            <Link
+              href='https://guides.data.gouv.fr/'
+              target='_blank'
+              className='font-medium hover:underline'
+            >
+              guides et documentation data.gouv.fr
+            </Link>{' '}
+            permet d'aller plus loin pour accompagner les collectivités dans leur démarche
+            d'ouverture des données.
+          </p>
+          <p>
+            Ainsi, pour les données sur les subventions, les collectivités sont appelées à se
+            conformer au schéma défini sur cette{' '}
+            <Link
+              href='https://schema.data.gouv.fr/scdl/subventions/'
+              target='_blank'
+              className='font-medium hover:underline'
+            >
+              page consacrée aux subventions
+            </Link>
+            .
+          </p>
+          <p>
+            Pour les marchés publics, les collectivités sont appelées à se conformer aux{' '}
+            <Link
+              href='https://schema.data.gouv.fr/139bercy/format-commande-publique/'
+              target='_blank'
+              className='font-medium hover:underline'
+            >
+              schémas des données essentielles de la commande publique
+            </Link>
+            .
+          </p>
+        </div>
       </article>
-      <h2 className='my-12 text-xl font-bold'>Questions fréquentes</h2>
-      <Accordion type='single' collapsible className='my-6'>
-        <AccordionItem value='item-1'>
-          <AccordionTrigger>
-            Pourquoi ma collectivité a une mauvaise note de transparence ?
-          </AccordionTrigger>
-          <AccordionContent>
-            {/* TODO - rédiger la réponse */}
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae assumenda perferendis
-            eveniet impedit sed aspernatur.
-          </AccordionContent>
-        </AccordionItem>
-        <AccordionItem value='item-2'>
-          <AccordionTrigger>
-            Que puis-je faire pour améliorer la transparence de ma collectivité ?
-          </AccordionTrigger>
-          <AccordionContent>
-            {/* TODO - rédiger la réponse */}
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae assumenda perferendis
-            eveniet impedit sed aspernatur.
-          </AccordionContent>
-        </AccordionItem>
-        <AccordionItem value='item-3'>
-          <AccordionTrigger>
-            Combien ça coûte d'ouvrir les données au grand public ?
-          </AccordionTrigger>
-          <AccordionContent>
-            {/* TODO - rédiger la réponse */}
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae assumenda perferendis
-            eveniet impedit sed aspernatur.
-          </AccordionContent>
-        </AccordionItem>
-        <AccordionItem value='item-4'>
-          <AccordionTrigger>
-            Les données publiées par Eclaireur Public sur ma collectivité sont erronées, comment les
-            faire corriger ?
-          </AccordionTrigger>
-          <AccordionContent>
-            {/* TODO - rédiger la réponse */}
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae assumenda perferendis
-            eveniet impedit sed aspernatur.
-          </AccordionContent>
-        </AccordionItem>
-      </Accordion>
+      <div className='mx-auto mb-20 w-full p-4 md:p-8 xl:max-w-[1128px] xl:p-0'>
+        <div className='hidden grid-cols-2 md:grid'>
+          <div className='mt-2.5 space-y-4'>
+            <h1 className='text-3xl font-bold'>Questions fréquentes</h1>
+            <div className='w-[350px] text-xl font-semibold md:block lg:w-[500px]'>
+              Consultez les réponses aux questions les plus courantes sur l'utilisation d'Éclaireur
+              Public
+            </div>
+            <Image
+              src={'/eclaireur/mascotte-faq.svg'}
+              alt='Image de la mascotte se posant des questions'
+              width={256}
+              height={128}
+            />
+          </div>
+          <div>
+            <Accordion type='multiple'>
+              <AccordionItem value='item-1'>
+                <AccordionTrigger>
+                  Pourquoi ma collectivité a une mauvaise note de transparence&nbsp;?
+                </AccordionTrigger>
+                <AccordionContent>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Recusandae, hic quaerat
+                  culpa sint sapiente, veritatis minima nam veniam quasi eius maiores velit soluta
+                  possimus sit deserunt ut dicta dolores dolore iste totam doloremque corrupti.
+                  Doloremque beatae ducimus, perspiciatis quas id animi reprehenderit minus
+                  asperiores dolor consequuntur, facilis incidunt omnis obcaecati.{' '}
+                </AccordionContent>
+              </AccordionItem>
+              <AccordionItem value='item-2'>
+                <AccordionTrigger>
+                  Que puis-je faire pour améliorer la transparence de ma collectivité&nbsp;?
+                </AccordionTrigger>
+                <AccordionContent>
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam accusantium
+                  illum amet magnam, expedita in autem incidunt deserunt, dolor laborum aperiam odit
+                  repellat rem facilis aspernatur totam! Eaque illo eum cumque ipsum nemo recusandae
+                  assumenda.{' '}
+                </AccordionContent>
+              </AccordionItem>
+              <AccordionItem value='item-3'>
+                <AccordionTrigger>
+                  Combien ça coûte d'ouvrir les données au grand public&nbsp;?
+                </AccordionTrigger>
+                <AccordionContent>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint impedit doloremque
+                  laudantium vel iure quos corrupti, quaerat voluptatem aliquid aut quod rerum
+                  commodi voluptate amet dignissimos qui autem sunt! Accusantium esse nisi
+                  praesentium delectus totam voluptatem distinctio laboriosam cumque modi!
+                </AccordionContent>
+              </AccordionItem>
+              <AccordionItem value='item-4'>
+                <AccordionTrigger>
+                  Les données publiées par Eclaireur Public sur ma collectivité sont erronées,
+                  comment les faire corriger&nbsp;?
+                </AccordionTrigger>
+                <AccordionContent>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed dolorem eius quo
+                  cumque voluptatem. Voluptas labore ex ducimus eius veritatis quae? Consectetur
+                  enim dicta nam ipsum. Fugiat molestiae assumenda explicabo.
+                </AccordionContent>
+              </AccordionItem>
+            </Accordion>
+          </div>
+        </div>
+
+        <div className='md:hidden'>
+          <h1 className='mb-4 text-3xl font-bold'>Questions fréquentes</h1>
+          <Accordion type='single' collapsible>
+            <AccordionItem value='item-1'>
+              <AccordionTrigger>
+                Pourquoi ma collectivité a une mauvaise note de transparence&nbsp;?
+              </AccordionTrigger>
+              <AccordionContent>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Recusandae, hic quaerat
+                culpa sint sapiente, veritatis minima nam veniam quasi eius maiores velit soluta
+                possimus sit deserunt ut dicta dolores dolore iste totam doloremque corrupti.
+                Doloremque beatae ducimus, perspiciatis quas id animi reprehenderit minus asperiores
+                dolor consequuntur, facilis incidunt omnis obcaecati.{' '}
+              </AccordionContent>
+            </AccordionItem>
+            <AccordionItem value='item-2'>
+              <AccordionTrigger>
+                Que puis-je faire pour améliorer la transparence de ma collectivité&nbsp;?
+              </AccordionTrigger>
+              <AccordionContent>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam accusantium illum
+                amet magnam, expedita in autem incidunt deserunt, dolor laborum aperiam odit
+                repellat rem facilis aspernatur totam! Eaque illo eum cumque ipsum nemo recusandae
+                assumenda.{' '}
+              </AccordionContent>
+            </AccordionItem>
+            <AccordionItem value='item-3'>
+              <AccordionTrigger>
+                Combien ça coûte d'ouvrir les données au grand public&nbsp;?
+              </AccordionTrigger>
+              <AccordionContent>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint impedit doloremque
+                laudantium vel iure quos corrupti, quaerat voluptatem aliquid aut quod rerum commodi
+                voluptate amet dignissimos qui autem sunt! Accusantium esse nisi praesentium
+                delectus totam voluptatem distinctio laboriosam cumque modi!
+              </AccordionContent>
+            </AccordionItem>
+            <AccordionItem value='item-4'>
+              <AccordionTrigger>
+                Les données publiées par Eclaireur Public sur ma collectivité sont erronées, comment
+                les faire corriger&nbsp;?
+              </AccordionTrigger>
+              <AccordionContent>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed dolorem eius quo
+                cumque voluptatem. Voluptas labore ex ducimus eius veritatis quae? Consectetur enim
+                dicta nam ipsum. Fugiat molestiae assumenda explicabo.
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+        </div>
+      </div>
     </main>
   );
 }
