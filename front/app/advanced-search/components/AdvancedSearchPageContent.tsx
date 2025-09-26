25afadd694d9e0096f9be4e48db0f808c8b59a71
Antoine Quesnel (perso) <antoine.quesnel@hotmail.fr>
2025-09-26 17:19:42 +0200

    feat: Ajouter les filtres en version mobile

--- front/app/advanced-search/components/AdvancedSearchPageContent.tsx
+++ front/app/advanced-search/components/AdvancedSearchPageContent.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from 'react';
 
+import { FilterButton } from '#app/advanced-search/components/Filters/FilterButton';
 import type { AdvancedSearchCommunity } from '@/app/models/community';
 import type { Table } from '@tanstack/react-table';
 
@@ -11,17 +12,21 @@ import { Filters } from './Filters/Filters';
 import { TableProvider } from './TableContext';
 import { ViewOptionsButton } from './ViewOptionsButton';
 
-export default function AdvancedSearchPageContent() {
+export default function AdvancedFilterPageContent() {
   const [table, setTable] = useState<Table<AdvancedSearchCommunity> | null>(null);
-
   return (
     <TableProvider table={table} setTable={setTable}>
-      <div className='flex items-end justify-between'>
+      {/* Desktop */}
+      <div className='flex items-end justify-between max-lg:hidden'>
         <Filters />
         <div className='flex items-end gap-2'>
           <ViewOptionsButton table={table} />
           <DownloadingButton />
         </div>
+      </div>
+      {/* Mobile */}
+      <div className='flex justify-end pr-3 lg:hidden'>
+        <FilterButton />
       </div>
       <CommunitiesTableWithLoader />
     </TableProvider>
---
+++ front/app/advanced-search/components/Filters/FilterButton.tsx
@@ -0,0 +1,47 @@
+'use client';
+
+import { useState } from 'react';
+
+import { ActionButton } from '#components/ui/action-button';
+import { Button } from '#components/ui/button';
+import { Dialog, DialogContent, DialogTitle, DialogTrigger } from '#components/ui/dialog';
+import { ListFilter } from 'lucide-react';
+
+import FilterModal from './FilterModal';
+
+type FilterButtonProps = {
+  className?: string;
+};
+
+export function FilterButton({ className }: FilterButtonProps) {
+  const [isFilterModalOpen, setFilterModalOpen] = useState(false);
+
+  function closeModal() {
+    setFilterModalOpen(false);
+  }
+
+  return (
+    <div className={`flex gap-2 ${className}`}>
+      <Dialog open={isFilterModalOpen} onOpenChange={setFilterModalOpen}>
+        <DialogTrigger asChild>
+          <ActionButton variant='outline' icon={<ListFilter className='h-6 w-6' />} />
+        </DialogTrigger>
+        <DialogContent className='w-[95%] rounded-3xl sm:max-w-md [&>button]:left-4 [&>button]:right-auto [&>button]:flex [&>button]:h-12 [&>button]:w-12 [&>button]:items-center [&>button]:justify-center [&>button]:rounded-bl-none [&>button]:rounded-br-lg [&>button]:rounded-tl-lg [&>button]:rounded-tr-none [&>button]:border [&>button]:border-primary [&>button]:bg-white [&>button]:text-primary [&>button]:hover:bg-primary [&>button]:hover:text-primary-foreground'>
+          <DialogTitle className='text-center'>
+            <div className='flex w-full'>
+              <h3 className='absolute left-1/2 -translate-x-1/2'>Filtres</h3>
+              <Button
+                variant='link'
+                size='sm'
+                className='ml-auto text-base text-xs font-medium text-danger'
+              >
+                Effacer tout
+              </Button>
+            </div>
+          </DialogTitle>
+          <FilterModal closeModal={closeModal} />
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+}
