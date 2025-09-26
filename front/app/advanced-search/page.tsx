22ac5d17fc4d0fee6792e214f097b9470e6ef6a1
Antoine Quesnel (perso) <antoine.quesnel@hotmail.fr>
2025-09-26 10:01:09 +0200

    feat: Maj header

--- front/app/advanced-search/page.tsx
+++ front/app/advanced-search/page.tsx
@@ -2,25 +2,29 @@ import { Suspense } from 'react';
 
 import type { Metadata } from 'next';
 
+import { SectionHeader } from '#app/components/SectionHeader';
 import Loading from '#components/ui/Loading';
 
 import AdvancedSearchPageContent from './components/AdvancedSearchPageContent';
-import GoBackHome from './components/GoBackHome';
+
+const title = 'Recherche Avancée';
+const description =
+  'Recherchez les dernières données de dépenses publiques de votre collectivité locale.';
 
 export const metadata: Metadata = {
-  title: 'Recherche avancée',
-  description:
-    'Télécharger les données des collectivités sélectionnées par les filtres de recherche.',
+  title: title,
+  description: description,
 };
 
 export default function Page() {
   return (
-    <div className='global-margin my-20 flex flex-col gap-x-10 gap-y-5'>
-      <GoBackHome />
-      <h1 className='text-2xl font-bold'>Recherche Avancée</h1>
-      <Suspense fallback={<Loading />}>
-        <AdvancedSearchPageContent />
-      </Suspense>
-    </div>
+    <main>
+      <SectionHeader sectionTitle={title} sectionSubTitle={description} displayGoBack={true} />
+      <div className='global-margin my-20 flex flex-col gap-x-10 gap-y-5'>
+        <Suspense fallback={<Loading />}>
+          <AdvancedSearchPageContent />
+        </Suspense>
+      </div>
+    </main>
   );
 }
