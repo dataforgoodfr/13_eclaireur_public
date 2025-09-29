9e68bc957532c06e1d22b76633c080e05433acc4
GF <guillaume.fontanet@gmx.fr>
2025-09-29 18:30:39 +0200

    Correctif tests

--- tests/back/datasets/test_marches.py
+++ tests/back/datasets/test_marches.py
@@ -34,16 +34,17 @@ def test_marches_public_dataframes():
     direct_df = mp._read_parse_file(
         file_metadata=None, raw_filename=FIXTURES_DIRECTORY / "marche_direct.json"
     )
-    assert direct_df.shape == (3, 6)
+
+    assert direct_df.shape == (2, 5)
     assert "acheteur_id" in direct_df.columns
     assert "a_02" in direct_df["acheteur_id"].tolist()
-    assert "titulaire_id" in direct_df.columns
-    assert "id_3" in direct_df["titulaire_id"].tolist()
+    assert "titulaires" in direct_df.columns
+    assert "[{'typeIdentifiant': 'SIRET', 'id': 'id_1'}]" in direct_df["titulaires"].to_list()
     pdtesting.assert_series_equal(
-        direct_df["montant"], pd.Series([500, 40, 40], name="montant")
+        direct_df["montant"], pd.Series([500, 40], name="montant")
     )
     pdtesting.assert_series_equal(
-        direct_df["countTitulaires"], pd.Series([1, 2, 2], name="countTitulaires")
+        direct_df["countTitulaires"], pd.Series([1, 2], name="countTitulaires")
     )
     # Remove the interim.json file created by mp workflow
     os.remove(FIXTURES_DIRECTORY / "interim.json")
@@ -52,16 +53,16 @@ def test_marches_public_dataframes():
     nested_df = mp._read_parse_file(
         file_metadata=None, raw_filename=FIXTURES_DIRECTORY / "marche_nested.json"
     )
-    assert nested_df.shape == (3, 6)
+    assert nested_df.shape == (2, 5)
     assert "acheteur_id" in nested_df.columns
     assert "a_02" in nested_df["acheteur_id"].tolist()
-    assert "titulaire_id" in nested_df.columns
-    assert "id_3" in nested_df["titulaire_id"].tolist()
+    assert "titulaires" in nested_df.columns
+    assert "[{'typeIdentifiant': 'SIRET', 'id': 'id_1'}]" in nested_df["titulaires"].to_list()
     pdtesting.assert_series_equal(
-        nested_df["montant"], pd.Series([200, 20, 20], name="montant")
+        nested_df["montant"], pd.Series([200, 20], name="montant")
     )
     pdtesting.assert_series_equal(
-        nested_df["countTitulaires"], pd.Series([1, 2, 2], name="countTitulaires")
+        nested_df["countTitulaires"], pd.Series([1, 2], name="countTitulaires")
     )
     # Remove the interim.json file created by mp workflow
     os.remove(FIXTURES_DIRECTORY / "interim.json")
