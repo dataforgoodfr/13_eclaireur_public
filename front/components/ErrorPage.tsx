6550891fa8dba08ef16b38edce745d04d4cdd5cf
Benjamin AUBRON <benjamin.aubron@outlook.com>
2025-09-30 16:06:38 +0200

    fix - vertical center error message

--- front/components/ErrorPage.tsx
+++ front/components/ErrorPage.tsx
@@ -11,29 +11,31 @@ type ErrorPageProps = {
 
 export default function ErrorPage({ title, description, errorType }: ErrorPageProps) {
   return (
-    <div className='flex min-h-screen flex-col items-center bg-primary px-4 pb-20 pt-28 md:pt-40'>
-      <div>
-        <h2 className='text-center text-3xl text-white md:text-5xl'>Ouuuups ! </h2>
-        <h1 className='text-center text-3xl text-white md:text-5xl'>{title}</h1>
+    <div className='relative -mb-20 min-h-screen bg-primary'>
+      <div className='absolute left-1/2 top-[calc(50%-40px)] flex -translate-x-1/2 -translate-y-1/2 flex-col items-center px-4'>
+        <div>
+          <h2 className='text-center text-3xl text-white md:text-5xl'>Ouuuups ! </h2>
+          <h1 className='text-center text-3xl text-white md:text-5xl'>{title}</h1>
+        </div>
+        <Image
+          src={`${errorType === '404' ? '/eclaireur/404.png' : '/eclaireur/500.png'}`}
+          width={430}
+          height={100}
+          alt="Image d'erreur"
+          className='w-[280px] md:w-[430px]'
+        />
+        <p className='w-[280px] text-center text-xl text-white md:w-[600px] md:text-2xl'>
+          {description}
+        </p>
+        <Link href={'/'}>
+          <Button
+            variant='action'
+            className='mt-8 bg-[#fbf79f] text-primary drop-shadow-[0_0_30px_rgba(255,221,51,0.8)] hover:bg-[#fbf79f]/80'
+          >
+            Revenir à la lumière
+          </Button>
+        </Link>
       </div>
-      <Image
-        src={`${errorType === '404' ? '/eclaireur/404.png' : '/eclaireur/500.png'}`}
-        width={430}
-        height={100}
-        alt="Image d'erreur"
-        className='w-[280px] md:w-[430px]'
-      />
-      <p className='w-[300px] text-center text-xl text-white md:w-[600px] md:text-2xl'>
-        {description}
-      </p>
-      <Link href={'/'}>
-        <Button
-          variant='action'
-          className='mt-8 bg-[#fbf79f] text-primary drop-shadow-[0_0_30px_rgba(255,221,51,0.8)] hover:bg-[#fbf79f]/80'
-        >
-          Revenir à la lumière
-        </Button>
-      </Link>
     </div>
   );
 }
