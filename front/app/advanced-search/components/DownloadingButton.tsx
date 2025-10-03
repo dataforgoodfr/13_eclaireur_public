3fd95d6d6dd702d2112607e8e9f293cbd5e53064
Antoine Quesnel (perso) <antoine.quesnel@hotmail.fr>
2025-10-03 12:18:39 +0200

    feat: maj display bouton télécharger

--- front/app/advanced-search/components/DownloadingButton.tsx
+++ front/app/advanced-search/components/DownloadingButton.tsx
@@ -3,7 +3,14 @@
 import Link from 'next/link';
 
 import { Button } from '#components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '#components/ui/dropdown-menu';
 import { createAdvancedSearchDownloadingURL } from '#utils/fetchers/advanced-search/download/downloadAdvancedSearch-client';
+import { ArrowDownToLine } from 'lucide-react';
 
 import { useFiltersParams } from '../hooks/useFiltersParams';
 import { useOrderParams } from '../hooks/useOrderParams';
@@ -14,8 +21,43 @@ export default function DownloadingButton() {
   const downloadingURL = createAdvancedSearchDownloadingURL(filters, order);
 
   return (
-    <Link href={downloadingURL} download={true} target='_blank'>
-      <Button variant='secondary'>Télécharger</Button>
-    </Link>
+    // <Link href={downloadingURL} download={true} target='_blank'>
+    //   <Button variant='secondary'>Télécharger</Button>
+    // </Link>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant='default'
+          className='rounded-bl-none rounded-br-lg rounded-tl-lg rounded-tr-none bg-primary hover:bg-primary/90 max-md:h-12 max-md:w-14 md:bg-secondary'
+        >
+          <ArrowDownToLine className='md:hidden' />
+          <span className='max-md:sr-only'>Télécharger</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className='min-w-[4rem]'>
+        <DropdownMenuItem>
+          <Link
+            className='flex w-full items-center hover:underline'
+            href={downloadingURL}
+            download={true}
+            target='_blank'
+          >
+            <ArrowDownToLine className='me-2' />
+            .csv
+          </Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <Link
+            className='flex w-full items-center hover:underline'
+            href={downloadingURL}
+            download={true}
+            target='_blank'
+          >
+            <ArrowDownToLine className='me-2' />
+            .xlsx
+          </Link>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 }
