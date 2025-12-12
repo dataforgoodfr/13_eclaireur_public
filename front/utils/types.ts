ef81436048134ba186683e9fd21b51468ce57263
Antoine Quesnel <antoine.quesnel@hotmail.fr>
2025-12-12 16:28:44 +0100

    remove unnused community type

--- front/utils/types.ts
+++ front/utils/types.ts
@@ -2,16 +2,6 @@ export enum CommunityType {
   Region = 'REG',
   Departement = 'DEP',
   Commune = 'COM',
-  /** Metropole au sens de la ville principale d une region geographique */
-  Metropole = 'MET',
-  /** Collectivite territoriale unique (ex: Corse, Martinique, Guyane) */
-  CTU = 'CTU',
-  /** Communaute d'agglomerations */
-  CA = 'CA',
-  /** Communaute de communes */
-  CC = 'CC',
-  /** Etablissement public territorial */
-  EPT = 'EPT',
   /** Groupement */
   GRP = 'GRP',
 }
