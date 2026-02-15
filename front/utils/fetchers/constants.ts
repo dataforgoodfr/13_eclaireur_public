dataforgoodfr/13_eclaireur_public

commit d1172315516c0b57695a393089ab021cd14d153f

[FRONT] features interpellate v2

front/utils/fetchers/constants.ts
@@ -8,4 +8,5 @@ export enum DataTable {
   Subventions = 'subventions',
   MarchesPublics = 'marches_publics',
   Elus = 'elus',
+  Contacts = 'communties_contacts',
 }

front/app/(visualiser)/interpeller/[siren]/step1/page.tsx
@@ -28,7 +28,7 @@ export default async function InterpellateStep1({
       <article className='px-8 py-12 outline'>
         <div className='flex justify-between'>
           <BadgeCommunity isExemplaire={true} />
-          <BudgetGlobal communityName='Nantes' />
+          <BudgetGlobal communitySiren={siren} />
         </div>
         <div className='mt-10 flex flex-col justify-between md:flex-row'>
           <MiniFicheCommunity communitySiren={siren} />

front/app/(visualiser)/interpeller/[siren]/step2/page.tsx
@@ -1,9 +1,10 @@
 import Link from 'next/link';
 
-import ElectedPolician from '@/components/ElectedPoliticians/SinglePoliticianCard';
+import ContactList from '@/components/Contacts/ContactList';
+import ButtonBackAndForth from '@/components/Interpellate/ButtonBackAndForth';
 import Stepper from '@/components/Interpellate/Stepper';
-import { buttonVariants } from '@/components/ui/button';
-import { ChevronLeft, ChevronRight } from 'lucide-react';
+import { fetchCommunities } from '@/utils/fetchers/communities/fetchCommunities-server';
+import { fetchContacts } from '@/utils/fetchers/contacts/fetchContacts-server';
 
 export default async function InterpellateStep2({
   params,
@@ -11,60 +12,73 @@ export default async function InterpellateStep2({
   params: Promise<{ siren: string }>;
 }) {
   const { siren } = await params;
+  async function getContacts(siren: string) {
+    return await fetchContacts({ filters: { siren } });
+  }
+  async function getCommunity(siren: string) {
+    const communitiesResults = await fetchCommunities({ filters: { siren } });
+    if (communitiesResults.length === 0) {
+      throw new Error(`Community doesnt exist with siren ${siren}`);
+    }
+    return communitiesResults[0];
+  }
+  const community = await getCommunity(siren);
+  const communityName = community.nom;
+
+  const contacts = await getContacts(siren);
+  console.log('contacts => ', contacts);
+  const emailContacts = contacts.filter((elt) => elt.type_contact === 'MAIL');
+  let emailContactsLen = emailContacts.length;
+  const formContact = contacts.filter((elt) => elt.type_contact === 'WEB');
+
   return (
     <section id='interpellation-step2' className='my-16'>
       <article>
         <Stepper currentStep={2} />
+        {emailContactsLen > 0 && (
+          <h2 className='mb-12 mt-6 text-center text-2xl font-bold'>
+            {communityName} <br />
+            Choisissez le ou les contacts ou élu.e.s à interpeller
+          </h2>
+        )}
+        {!emailContactsLen && (
+          <h3 className='mb-12 mt-6 text-center text-xl font-bold'>
+            Oops ! Nous n’avons pas de contact direct ou générique avec la collectivité
+            sélectionnée.
+            <br />
+            <span className='font-normal'>
+              Cependant, vous pouvez toujours agir pour faire valoir la transparence des données
+              publiques !
+            </span>
+          </h3>
+        )}
 
-        <h2 className='mb-12 mt-6 text-center text-2xl font-bold'>
-          Choisissez les élu.e.s à interpeller
-        </h2>
-        <ul className='flex gap-4'>
-          <li>
-            <ElectedPolician
-              name='toto'
-              photoSrc='https://placehold.co/200/png'
-              fonction='Maire'
-              email='johanna.rolland@nantes.com'
+        {/* <ul className='flex flex-wrap gap-4'>
+          {emailContacts && (
+            <ContactsList
+              contactsitems={emailContacts.map((contact) => ({
+                name: contact.nom,
+                email: contact.contact,
+                photoSrc: 'https://placehold.co/200/png',
+                fonction: 'contact générique',
+              }))}
             />
-          </li>
-          <li>
-            <ElectedPolician
-              name='toti'
-              photoSrc=''
-              fonction='Maire'
-              email='johanna.rolland@nantes.com'
-            />
-          </li>
-          <li>
-            <ElectedPolician
-              name='Jackie'
-              photoSrc=''
-              fonction='Adjointe au maire'
-              email='jaqueline-d@nantes.com'
-            />
-          </li>
-        </ul>
+          )}
+        </ul> */}
+        {/* {emailContacts && <ContactsDisplayZone items={emailContacts} siren={siren} />} */}
+        {emailContacts && (
+          <ul className='flex flex-wrap gap-4'>
+            <ContactList contacts={emailContacts} siren={siren} />
+          </ul>
+        )}
       </article>
       <div className='my-12 flex justify-center gap-4'>
-        <Link
-          href={`/interpeller/${siren}/step1`}
-          className={buttonVariants({
-            variant: 'outline',
-            className: 'min-w-[200] bg-black text-white',
-          })}
-        >
-          <ChevronLeft /> Revenir
-        </Link>
-        <Link
-          href={`/interpeller/${siren}/step3`}
-          className={buttonVariants({
-            variant: 'outline',
-            className: 'min-w-[200] bg-black text-white',
-          })}
-        >
-          Continuer <ChevronRight />
-        </Link>
+        <ButtonBackAndForth linkto={`/interpeller/${siren}/step1`} direction='back'>
+          Revenir
+        </ButtonBackAndForth>
+        <ButtonBackAndForth linkto={`/interpeller/${siren}/step3`} direction='forth'>
+          Continuer
+        </ButtonBackAndForth>
       </div>
     </section>
   );

front/app/(visualiser)/interpeller/[siren]/step3/page.tsx
@@ -15,7 +15,6 @@ export default async function InterpellateStep3({
 
         <h2 className='mb-12 mt-6 text-center text-2xl font-bold'>Envoyez votre message</h2>
         <InterpellateForm
-          emails={['olivier.pretre@gmx.fr', 'toto']}
           missingData=''
           communityParam={siren}
         />

front/app/(visualiser)/interpeller/page.tsx
@@ -19,7 +19,7 @@ export default function Page() {
       <Stepper currentStep={1} />
       <article className='my-6 flex flex-col justify-start'>
         <h2 className='my-6 text-center text-2xl font-bold'>Trouver une collectivité</h2>
-        <div className='ml-16 min-w-[400] self-center'>
+        <div className='min-w-[400] w-4/5 mx-auto flex justify-center'>
           <SearchBar onSelect={({ siren }) => goToStep1(siren)} />
         </div>
       </article>

front/app/api/interpellate/route.ts
@@ -21,6 +21,7 @@ export async function POST(request: Request) {
   if (success && data) {
     ({ firstname, lastname, email, emails, object, message } = data);
   }
+  console.log('message !!!! => ', message);
 
   // check out Zod's .flatten() method for an easier way to process errors
   let zodErrors = {};
@@ -49,6 +50,7 @@ export async function POST(request: Request) {
     to: process.env.MY_EMAIL,
     cc: process.env.CC_EMAIL_MESSAGE,
     subject: `|| ECLAIREUR PUBLIC || Message de ${firstname} ${lastname} (${email})`,
+    // html: typeof message === 'string' ? message : '',
     html: message,
   };
 

front/app/models/communityAccount.ts
@@ -15,3 +15,28 @@ export type Community = {
   dette: boolean;
   annee: number;
 };
+export type CommunityAccount = {
+  /** Primary key [char9] */
+  siren: string | null;
+  region: string;
+  total_produits: TransparencyScore;
+  total_charges: number;
+  resultat: number;
+  subventions: number;
+  ressources_invest: number;
+  emploi_invest: number;
+  ebf: number;
+  caf: number;
+  population: string;
+  dette: number;
+  annee: number;
+  insee_commune: string;
+  dept: string;
+  type: string;
+  region_clean: string;
+  dep_clean: string;
+  insee_commune_clean: string;
+  nom_reg: string;
+  nom_dept: string;
+  nom_com: string;
+};

front/app/models/contact.ts
@@ -1,0 +1,10 @@
+export type Contact = {
+  /** Primary key [char9] */
+  siren: string;
+  /** Primary key */
+  type: string;
+  nom: string;
+  code_insee: string;
+  contact: string;
+  type_contact: string;
+};

commit 4c1fe81e6032a7c73180993c8dd8ce88c6a89e07

[FRONT] fix(db): correct contacts table name typo
Fix typo in DataTable enum: 'communties_contacts' -> 'communities_contacts'

This typo was introduced in commit d1172315 and caused queries to the
contacts table to fail with 'relation does not exist' error, resulting
in HTTP 500 on /interpeller/[siren]/step2 route.

no_jira

front/utils/fetchers/constants.ts
@@ -9,5 +9,5 @@ export enum DataTable {
   MarchesPublics = 'marches_publics',
   Elus = 'elus',
   Bareme = 'bareme',
-  Contacts = 'communties_contacts',
+  Contacts = 'communities_contacts',
 }